3db05078c000aa6643f08a9b576a7556fbeab3e8
Yingbei-Tong <tybalex@gmail.com>
2021-04-08 12:50:57 -0700

    remove comments

--- src/training_controller/training_controller.py
+++ src/training_controller/training_controller.py
@@ -118,62 +118,6 @@ def run_job(job_details):
     api_instance.create_namespaced_job(body=job, namespace="default")
 
 
-"""
-async def run(loop, queue):
-    nc = NATS()
-
-    async def error_cb(e):
-        logging.warning("Error: {}".format(str(e)))
-
-    async def closed_cb():
-        logging.warning("Closed connection to NATS")
-        await asyncio.sleep(0.1, loop=loop)
-        loop.stop()
-
-    async def on_disconnect():
-        logging.warning("Disconnected from NATS")
-
-    async def reconnected_cb():
-        logging.warning(
-            "Reconnected to NATS at nats://{}".format(nats.connected_url.netloc)
-        )
-
-    async def subscribe_handler(msg):
-        subject = msg.subject
-        reply = msg.reply
-        payload_data = msg.data.decode()
-        logging.info("Received log messages.")
-        await queue.put(payload_data)
-
-    options = {
-        "loop": loop,
-        "error_cb": error_cb,
-        "closed_cb": closed_cb,
-        "reconnected_cb": reconnected_cb,
-        "disconnected_cb": on_disconnect,
-        "servers": [NATS_SERVER_URL],
-    }
-
-    try:
-        await nc.connect(**options)
-    except Exception as e:
-        logging.error(str(e))
-
-    logging.info(f"Connected to NATS at {nc.connected_url.netloc}...")
-
-    def signal_handler():
-        if nc.is_closed:
-            return
-        logging.warning("Disconnecting...")
-        loop.create_task(nc.close())
-
-    for sig in ("SIGINT", "SIGTERM"):
-        loop.add_signal_handler(getattr(signal, sig), signal_handler)
-
-    await nc.subscribe("train", "", subscribe_handler)
-"""
-
-
 async def clear_jobs(signals_queue):
     namespace = "default"
     state = "finished"

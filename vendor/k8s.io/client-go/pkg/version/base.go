5ae280be0d5799e37339d0064e2a16a7b31693d7
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2021-04-30 00:47:37 +0200

    go vendoring

---
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	gitMajor = "1"
+	gitMinor = "20"
+	gitVersion   = "v1.20.2-k3s1"
+	gitCommit    = "426bcc7128ac615e92181e084eafdef3b0206d76"
+	gitTreeState = "clean"
+	buildDate = "2021-01-13T19:38:13Z"
+)

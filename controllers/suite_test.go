bf73115d127d525c8d126d97e4d5603cd2c8e392
Joe Kralicky <joe.kralicky@suse.com>
2021-08-17 16:54:54 -0400

    remove unused constants

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -53,9 +53,6 @@ var (
 )
 
 const (
-	crName         = "test-opnicluster"
-	crNamespace    = "opnicluster-test"
-	laClusterName  = "test-oc-logadapter"
 	systemdLogPath = "/var/log/testing"
 	openrcLogPath  = "/var/test/alternate.log"
 	openrcLogDir   = "/var/test"

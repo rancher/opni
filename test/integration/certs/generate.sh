73b51fc4e93ebebf7be1ccf4bb44580f61e9bf53
Joe Kralicky <joe.kralicky@suse.com>
2022-01-16 20:30:22 -0500

    Remove old test data

--- test/integration/certs/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f root_ca.crt root_ca.key leaf.crt leaf.key
-step certificate create root-ca --profile root-ca root_ca.crt root_ca.key --kty OKP --curve Ed25519 --insecure --no-password
-step certificate create leaf --profile leaf --ca root_ca.crt --ca-key root_ca.key leaf.crt leaf.key --kty OKP --curve Ed25519 --insecure --no-password

7099bc5aa2eccf5b478e7c5721d4678eba34d62c
Joe Kralicky <joe.kralicky@suse.com>
2022-01-28 15:57:28 -0500

    Refactor openid auth configuration

--- pkg/auth/openid/openid.go
+++ pkg/auth/openid/openid.go
@@ -2,7 +2,11 @@ package openid
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
+	"path"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
@@ -21,13 +25,26 @@ const (
 	TokenKey = "token"
 )
 
+type WellKnownConfiguration struct {
+	Issuer           string `json:"issuer"`
+	AuthEndpoint     string `json:"authorization_endpoint"`
+	TokenEndpoint    string `json:"token_endpoint"`
+	UserinfoEndpoint string `json:"userinfo_endpoint"`
+	JwksUri          string `json:"jwks_uri"`
+}
+
 type OpenidConfig struct {
-	JwkSetUrl string `mapstructure:"jwkSetUrl"`
+	// The OpenID issuer identifier URL. The provider's OpenID configuration
+	// will be fetched from <Issuer>/.well-known/openid-configuration.
+	// The issuer URL must exactly match the issuer in the well-known
+	// configuration, including any trailing slashes.
+	Issuer string `mapstructure:"server"`
 }
 
 type OpenidMiddleware struct {
-	keyRefresher *jwk.AutoRefresh
-	conf         *OpenidConfig
+	keyRefresher    *jwk.AutoRefresh
+	conf            *OpenidConfig
+	wellKnownConfig *WellKnownConfiguration
 }
 
 var _ auth.Middleware = (*OpenidMiddleware)(nil)
@@ -40,7 +57,27 @@ func New(config v1beta1.AuthProviderSpec) (auth.Middleware, error) {
 	if err := mapstructure.Decode(config.Options, m.conf); err != nil {
 		return nil, err
 	}
-	m.keyRefresher.Configure(m.conf.JwkSetUrl)
+	issuerURL, err := url.Parse(m.conf.Issuer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse openid issuer URL: %v", err)
+	}
+	if issuerURL.Scheme != "https" {
+		return nil, fmt.Errorf("openid issuer URL must have https scheme")
+	}
+	if !strings.HasSuffix(issuerURL.Path, ".well-known/openid-configuration") {
+		issuerURL.Path = path.Join(issuerURL.Path, ".well-known/openid-configuration")
+	}
+
+	wellKnownCfg, err := fetchWellKnownConfig(issuerURL.String())
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch openid configuration: %v", err)
+	}
+	if wellKnownCfg.Issuer != m.conf.Issuer {
+		return nil, fmt.Errorf("openid issuer URL mismatch (server returned %q, expected %q)",
+			wellKnownCfg.Issuer, m.conf.Issuer)
+	}
+	m.wellKnownConfig = wellKnownCfg
+	m.keyRefresher.Configure(wellKnownCfg.JwksUri)
 	return m, nil
 }
 
@@ -50,7 +87,7 @@ func (m *OpenidMiddleware) Description() string {
 
 func (m *OpenidMiddleware) Handle(c *fiber.Ctx) error {
 	lg := c.Context().Logger()
-	set, err := m.keyRefresher.Fetch(context.Background(), m.conf.JwkSetUrl)
+	set, err := m.keyRefresher.Fetch(context.Background(), m.wellKnownConfig.JwksUri)
 	if err != nil {
 		lg.Printf("failed to fetch JWK set: %v", err)
 		return c.SendStatus(fiber.StatusServiceUnavailable)
@@ -74,4 +111,17 @@ func (m *OpenidMiddleware) Handle(c *fiber.Ctx) error {
 	c.Request().Header.Del("Authorization")
 	c.Locals(rbac.UserIDKey, userID)
 	return c.Next()
+}
+
+func fetchWellKnownConfig(url string) (*WellKnownConfiguration, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var cfg WellKnownConfiguration
+	if err := json.NewDecoder(resp.Body).Decode(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
 }

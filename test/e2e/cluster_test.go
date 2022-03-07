e92a5e6621b69f26b639763a5f8310a9668842d6
Dan Bason <colonel.dan@gmail.com>
2022-03-08 11:27:25 +1300

    Update config for v0.3.1

--- test/e2e/cluster_test.go
+++ test/e2e/cluster_test.go
@@ -113,7 +113,7 @@ var _ = Describe("OpniCluster E2E Test", Label("e2e"), func() {
 					Namespace: clusterCrNamespace,
 				},
 				Spec: v1beta1.OpniClusterSpec{
-					Version:            "v0.3.0",
+					Version:            "v0.3.1",
 					DeployLogCollector: pointer.BoolPtr(true),
 					Services: v1beta1.ServicesSpec{
 						GPUController: v1beta1.GPUControllerServiceSpec{

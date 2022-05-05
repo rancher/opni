fabfd696311a223f64e39e1af6a36b14a15bb966
Dan Bason <colonel.dan@gmail.com>
2022-05-05 16:34:05 +1200

    Update CRDs

--- magefile.go
+++ magefile.go
@@ -91,10 +91,10 @@ func ControllerGen() error {
 func CRDGen() error {
 	var commands []*exec.Cmd
 	commands = append(commands, exec.Command(mg.GoCmd(), "run", "sigs.k8s.io/kustomize/kustomize/v4",
-		"build", "./config/crd", "-o", "./packages/opni/charts/crds/crds.yaml",
+		"build", "./config/chart-crds", "-o", "./packages/opni/charts/crds/crds.yaml",
 	))
 	commands = append(commands, exec.Command(mg.GoCmd(), "run", "sigs.k8s.io/kustomize/kustomize/v4",
-		"build", "./config/crd", "-o", "./packages/opni-agent/charts/crds/crds.yaml",
+		"build", "./config/chart-crds", "-o", "./packages/opni-agent/opni-agent/charts/crds/crds.yaml",
 	))
 	for _, cmd := range commands {
 		buf := new(bytes.Buffer)

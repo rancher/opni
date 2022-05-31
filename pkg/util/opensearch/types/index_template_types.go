584babb5e485fe0a2033a4a23b646cc0f299796a
Dan Bason <colonel.dan@gmail.com>
2022-05-31 15:12:59 +1200

    Update index settings and add upgrade logic

--- pkg/util/opensearch/types/index_template_types.go
+++ pkg/util/opensearch/types/index_template_types.go
@@ -1,5 +1,12 @@
 package types
 
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/tidwall/gjson"
+)
+
 type IndexTemplateSpec struct {
 	TemplateName  string       `json:"-"`
 	IndexPatterns []string     `json:"index_patterns,omitempty"`
@@ -28,4 +35,53 @@ type PropertySettings struct {
 	Type    string `json:"type,omitempty"`
 	Format  string `json:"format,omitempty"`
 	Enabled *bool  `json:"enabled,omitempty"`
+}
+
+type GetIndexTemplateObject struct {
+	Name     string            `json:"name,omitempty"`
+	Template IndexTemplateSpec `json:"index_template,omitempty"`
+}
+
+type GetIndexTemplateResponse struct {
+	IndexTemplates []GetIndexTemplateObject `json:"index_templates,omitempty"`
+}
+
+func (t *TemplateSettingsSpec) UnmarshalJSON(data []byte) error {
+	value := gjson.GetBytes(data, "index")
+	var err error
+	if value.Exists() {
+		t.DefaultPipeline = value.Get("default_pipeline").String()
+		t.RolloverAlias = value.Get("opendistro.index_state_management.rollover_alias").String()
+		t.ISMPolicyID = value.Get("opendistro.index_state_management.policy_id").String()
+
+		numReplicas := value.Get("number_of_replicas")
+		if numReplicas.Exists() {
+			t.NumberOfReplicas, err = strconv.Atoi(numReplicas.String())
+			if err != nil {
+				return err
+			}
+		}
+		numShards := value.Get("number_of_shards")
+		if numShards.Exists() {
+			t.NumberOfShards, err = strconv.Atoi(value.Get("number_of_shards").String())
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	type settings TemplateSettingsSpec
+	tmp := settings{}
+	err = json.Unmarshal(data, &tmp)
+	if err != nil {
+		return err
+	}
+
+	t.DefaultPipeline = tmp.DefaultPipeline
+	t.RolloverAlias = tmp.RolloverAlias
+	t.ISMPolicyID = tmp.ISMPolicyID
+	t.NumberOfReplicas = tmp.NumberOfReplicas
+	t.NumberOfShards = tmp.NumberOfShards
+	return nil
 }

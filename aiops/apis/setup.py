519e0888d92b475f7ea31d9080f25ee9151f079d
Amartya Chakraborty <amartya.chakraborty@suse.com>
2022-06-30 16:09:01 -0700

    Update proto file

--- aiops/apis/setup.py
+++ aiops/apis/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-  name="opni-aiops-apis",
-  version="0.5.5",
+  name="opni_aiops_apis",
+  version="0.5.4",
   install_requires=["betterproto"],
   packages=find_packages(),
 )

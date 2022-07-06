f60812b6535491c1223980a1d85f39728323cf23
Amartya Chakraborty <amartya.chakraborty@suse.com>
2022-07-06 13:14:12 -0700

    Update setup.py script to point to opni_proto

--- aiops/apis/setup.py
+++ aiops/apis/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-  name="opni_aiops_apis",
+  name="opni_proto",
   version="0.5.4",
   install_requires=["betterproto~=1.2.0"],
   packages=find_packages(),

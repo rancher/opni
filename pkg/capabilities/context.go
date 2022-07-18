4f6c8367ab5237e6861890dc847a2b5844381e3d
Joe Kralicky <joe.kralicky@suse.com>
2022-07-18 17:55:27 -0400

    disconnect agent streams upon relevant capability loss

---
+++ pkg/capabilities/context.go
@@ -0,0 +1,108 @@
+package capabilities
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	corev1 "github.com/rancher/opni/pkg/apis/core/v1"
+	"github.com/rancher/opni/pkg/storage"
+	"go.uber.org/atomic"
+)
+
+var (
+	ErrObjectDeleted      = errors.New("object deleted")
+	ErrCapabilityNotFound = errors.New("capability not found")
+	ErrEventChannelClosed = errors.New("event channel closed")
+)
+
+type capabilityContext[T corev1.Capability[T], U corev1.MetadataAccessor[T]] struct {
+	base context.Context
+
+	eventC       <-chan storage.WatchEvent[U]
+	done         chan struct{}
+	capabilities []T
+	err          *atomic.Error
+}
+
+// Returns a context that listens on a watch event channel and closes its
+// Done channel when the watched object loses one of the specified capabilities,
+// or if the object is deleted. This context should have exclusive read access
+// to the event channel to avoid missing events.
+// If the list of capabilities is empty, the context will close its Done
+// channel if any capabilities are deleted.
+// Specific capabilities listed will be ignored if the context does not have
+// them. The context must have previously had one of the capabilities and lose
+// it to cause the context to close its Done channel.
+func NewContext[T corev1.Capability[T], U corev1.MetadataAccessor[T]](
+	base context.Context,
+	eventC <-chan storage.WatchEvent[U],
+	capabilities ...T,
+) (context.Context, context.CancelFunc) {
+	base, ca := context.WithCancel(base)
+	cc := &capabilityContext[T, U]{
+		base:         base,
+		eventC:       eventC,
+		done:         make(chan struct{}),
+		capabilities: capabilities,
+		err:          atomic.NewError(nil),
+	}
+	go cc.watch()
+	return cc, ca
+}
+
+func (cc *capabilityContext[T, U]) watch() {
+	defer close(cc.done)
+	for {
+		select {
+		case <-cc.base.Done():
+			cc.err.Store(cc.base.Err())
+			return
+		case event, ok := <-cc.eventC:
+			if !ok {
+				cc.err.Store(ErrEventChannelClosed)
+				return
+			}
+			switch event.EventType {
+			case storage.WatchEventDelete:
+				cc.err.Store(ErrObjectDeleted)
+				return
+			case storage.WatchEventPut:
+				if len(cc.capabilities) == 0 {
+					// check if any capabilities were lost
+					for _, c := range event.Previous.GetCapabilities() {
+						if !Has(event.Current, c) {
+							cc.err.Store(fmt.Errorf("%w: %s", ErrCapabilityNotFound, c))
+							return
+						}
+					}
+				} else {
+					// check if specific capabilities were lost
+					for _, c := range cc.capabilities {
+						if Has(event.Previous, c) && !Has(event.Current, c) {
+							cc.err.Store(fmt.Errorf("%w: %s", ErrCapabilityNotFound, c))
+							return
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func (cc *capabilityContext[T, U]) Deadline() (time.Time, bool) {
+	return cc.base.Deadline()
+}
+
+func (cc *capabilityContext[T, U]) Done() <-chan struct{} {
+	return cc.done
+}
+
+func (cc *capabilityContext[T, U]) Err() error {
+	return cc.err.Load()
+}
+
+func (cc *capabilityContext[T, U]) Value(key any) any {
+	return cc.base.Value(key)
+}
--- pkg/gateway/stream.go
+++ pkg/gateway/stream.go
@@ -2,33 +2,43 @@ package gateway
 
 import (
 	"context"
+	"errors"
 
 	"github.com/kralicky/totem"
 	"github.com/rancher/opni/pkg/agent"
+	corev1 "github.com/rancher/opni/pkg/apis/core/v1"
 	streamv1 "github.com/rancher/opni/pkg/apis/stream/v1"
+	"github.com/rancher/opni/pkg/auth/cluster"
+	"github.com/rancher/opni/pkg/capabilities"
+	"github.com/rancher/opni/pkg/plugins/apis/apiextensions"
+	"github.com/rancher/opni/pkg/storage"
 	"github.com/rancher/opni/pkg/util"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/descriptorpb"
 )
 
 type remote struct {
-	services []*descriptorpb.ServiceDescriptorProto
+	services []*apiextensions.ServiceDescriptor
 	cc       *grpc.ClientConn
 }
 
 type StreamServer struct {
 	streamv1.UnsafeStreamServer
-	logger   *zap.SugaredLogger
-	handler  ConnectionHandler
-	services []util.ServicePack[any]
-	remotes  []remote
+	logger       *zap.SugaredLogger
+	handler      ConnectionHandler
+	clusterStore storage.ClusterStore
+	services     []util.ServicePack[any]
+	remotes      []remote
 }
 
-func NewStreamServer(handler ConnectionHandler, lg *zap.SugaredLogger) *StreamServer {
+func NewStreamServer(handler ConnectionHandler, clusterStore storage.ClusterStore, lg *zap.SugaredLogger) *StreamServer {
 	return &StreamServer{
-		logger:  lg.Named("grpc"),
-		handler: handler,
+		logger:       lg.Named("grpc"),
+		handler:      handler,
+		clusterStore: clusterStore,
 	}
 }
 
@@ -38,27 +48,82 @@ func (s *StreamServer) Connect(stream streamv1.Stream_ConnectServer) error {
 	for _, service := range s.services {
 		ts.RegisterService(service.Unpack())
 	}
+	id := cluster.StreamAuthorizedID(stream.Context())
+
+	ctx := stream.Context()
+	cluster, err := s.clusterStore.GetCluster(ctx, &corev1.Reference{
+		Id: id,
+	})
+	if err != nil {
+		return err
+	}
+
+	// find any remote streams that match the cluster's capabilities, or that
+	// don't require any capabilities. If the cluster loses any required
+	// capabilities once it is connected, the stream will be closed and the
+	// cluster will need to reconnect.
+	requiredCapabilities := []*corev1.ClusterCapability{}
 	for _, r := range s.remotes {
+		var services []*descriptorpb.ServiceDescriptorProto
+		for _, service := range r.services {
+			// check if the service requires a specific capability
+			if service.Options.RequireCapability != "" {
+				rc := capabilities.Cluster(service.Options.RequireCapability)
+				// if the cluster doesn't have the capability, skip the service
+				if !capabilities.Has(cluster, rc) {
+					continue
+				} else {
+					requiredCapabilities = append(requiredCapabilities, rc)
+				}
+			}
+			services = append(services, service.ServiceDescriptor)
+		}
+		if len(services) == 0 {
+			continue
+		}
+
 		streamClient := streamv1.NewStreamClient(r.cc)
-		splicedStream, err := streamClient.Connect(context.Background())
+		splicedStream, err := streamClient.Connect(ctx)
 		if err != nil {
 			return err
 		}
-		ts.Splice(splicedStream, r.services...)
+		ts.Splice(splicedStream, services...)
 	}
+
+	// set up a context which will be canceled when the cluster loses any
+	// required capabilities
+
+	eventC, err := s.clusterStore.WatchCluster(stream.Context(), cluster)
+	if err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+	var ca context.CancelFunc
+	ctx, ca = capabilities.NewContext(ctx, eventC, requiredCapabilities...)
+	defer ca()
+
 	cc, errC := ts.Serve()
 
-	ctx, ca := context.WithCancel(stream.Context())
-	defer ca()
 	go s.handler.HandleAgentConnection(ctx, agent.NewClientSet(cc))
 
-	err := <-errC
-	if err != nil {
+	select {
+	case err = <-errC:
+		if err != nil {
+			s.logger.With(
+				zap.Error(err),
+			).Info("agent stream disconnected")
+		}
+		return status.Error(codes.Unavailable, err.Error())
+	case <-ctx.Done():
 		s.logger.With(
-			zap.Error(err),
-		).Info("agent stream disconnected")
+			zap.Error(ctx.Err()),
+		).Info("agent stream closing")
+		err := ctx.Err()
+		if errors.Is(err, capabilities.ErrObjectDeleted) ||
+			errors.Is(err, capabilities.ErrCapabilityNotFound) {
+			return status.Error(codes.FailedPrecondition, err.Error())
+		}
+		return status.Error(codes.Unavailable, err.Error())
 	}
-	return err
 }
 
 func (s *StreamServer) RegisterService(desc *grpc.ServiceDesc, impl any) {
@@ -70,9 +135,9 @@ func (s *StreamServer) RegisterService(desc *grpc.ServiceDesc, impl any) {
 	s.services = append(s.services, util.PackService(desc, impl))
 }
 
-func (s *StreamServer) AddRemote(cc *grpc.ClientConn, services []*descriptorpb.ServiceDescriptorProto) error {
+func (s *StreamServer) AddRemote(cc *grpc.ClientConn, services *apiextensions.ServiceDescriptorList) error {
 	s.remotes = append(s.remotes, remote{
-		services: services,
+		services: services.Descriptors,
 		cc:       cc,
 	})
 	return nil

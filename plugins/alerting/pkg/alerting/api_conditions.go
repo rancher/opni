22612958e93b8c0f3c2d274a61e93b24a8d5208e
Alexandre Lamarre <alexandre.lamarre@suse.com>
2022-08-01 11:18:33 -0400

    update condition & delete condition edge cases

--- plugins/alerting/pkg/alerting/api_conditions.go
+++ plugins/alerting/pkg/alerting/api_conditions.go
@@ -15,18 +15,13 @@ import (
 
 const conditionPrefix = "/alerting/conditions"
 
-func (p *Plugin) CreateAlertCondition(ctx context.Context, req *alertingv1alpha.AlertCondition) (*corev1.Reference, error) {
-	newId := uuid.New().String()
-	if err := p.storage.Get().Conditions.Put(ctx, path.Join(conditionPrefix, newId), req); err != nil {
-		return nil, err
-	}
-	// TODO check for alert type
+func setEndpointImplementationIfAvailable(p *Plugin, ctx context.Context, req *alertingv1alpha.AlertCondition, newId string) error {
 	if req.NotificationId != nil { // create the endpoint implementation
 		if req.Details == nil {
-			return nil, validation.Error("alerting notification details must be set if you specify a notification target")
+			return validation.Error("alerting notification details must be set if you specify a notification target")
 		}
 		if _, err := p.GetAlertEndpoint(ctx, &corev1.Reference{Id: *req.NotificationId}); err != nil {
-			return nil, err
+			return err
 		}
 
 		_, err := p.CreateEndpointImplementation(ctx, &alertingv1alpha.CreateImplementation{
@@ -35,16 +30,76 @@ func (p *Plugin) CreateAlertCondition(ctx context.Context, req *alertingv1alpha.
 			Implementation: req.Details,
 		})
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
+	return nil
+}
 
+func updateEndpointImplemetation(p *Plugin, ctx context.Context, req *alertingv1alpha.AlertCondition, id string) error {
+	if req.NotificationId != nil { // create the endpoint implementation
+		if req.Details == nil {
+			return validation.Error("alerting notification details must be set")
+		}
+		if _, err := p.GetAlertEndpoint(ctx, &corev1.Reference{Id: *req.NotificationId}); err != nil {
+			return err
+		}
+
+		_, err := p.UpdateEndpointImplementation(ctx, &alertingv1alpha.CreateImplementation{
+			NotificationId: &corev1.Reference{Id: *req.NotificationId},
+			ConditionId:    &corev1.Reference{Id: id},
+			Implementation: req.Details,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func handleUpdateEndpointImplementation(p *Plugin,
+	ctx context.Context,
+	id string,
+	existing *alertingv1alpha.AlertCondition,
+	new *alertingv1alpha.AlertCondition) error {
+	if existing.NotificationId == nil { // no implementation previously set
+		return setEndpointImplementationIfAvailable(p, ctx, new, id)
+	} else if new.NotificationId == nil { // delete implementation
+		_, err := p.DeleteEndpointImplementation(ctx, &corev1.Reference{Id: *existing.NotificationId})
+		return err
+	} else { // both are set update
+		return updateEndpointImplemetation(p, ctx, new, id)
+	}
+}
+
+func setupCondition(ctx context.Context, req *alertingv1alpha.AlertCondition, newId string) (*corev1.Reference, error) {
 	switch req.AlertType {
 	case &alertingv1alpha.AlertCondition_System{}: // opni system evaluates these conditions elsewhere in the code
 		return &corev1.Reference{Id: newId}, nil
 	default:
 		return nil, shared.AlertingErrNotImplemented
 	}
+}
+
+func deleteCondition(ctx context.Context, req *alertingv1alpha.AlertCondition, id string) error {
+	switch req.AlertType {
+	case &alertingv1alpha.AlertCondition_System{}: // opni system evaluates these conditions elsewhere in the code
+		return validation.Error("User should not be able to delete system alert conditions")
+	default:
+		return shared.AlertingErrNotImplemented
+	}
+}
+
+func (p *Plugin) CreateAlertCondition(ctx context.Context, req *alertingv1alpha.AlertCondition) (*corev1.Reference, error) {
+	newId := uuid.New().String()
+	if err := p.storage.Get().Conditions.Put(ctx, path.Join(conditionPrefix, newId), req); err != nil {
+		return nil, err
+	}
+	if err := setEndpointImplementationIfAvailable(p, ctx, req, newId); err != nil {
+		return nil, err
+	}
+
+	return setupCondition(ctx, req, newId)
 }
 
 func (p *Plugin) GetAlertCondition(ctx context.Context, ref *corev1.Reference) (*alertingv1alpha.AlertCondition, error) {
@@ -67,42 +122,15 @@ func (p *Plugin) UpdateAlertCondition(ctx context.Context, req *alertingv1alpha.
 	}
 	overrideLabels := req.UpdateAlert.Labels
 
-	// TODO  check if a previous notification implementation exists
-
-	// sub TODO check if we are removing it by passing no implementation
-
-	// sub TODO check if we are adding a new implementation from scratch
-
-	// default case : updating an existing implementation
-	if req.UpdateAlert.NotificationId != nil { // create the endpoint implementation
-		if req.UpdateAlert.Details == nil {
-			return nil, validation.Error("alerting notification details must be set")
-		}
-		if _, err := p.GetAlertEndpoint(ctx, &corev1.Reference{Id: *req.UpdateAlert.NotificationId}); err != nil {
-			return nil, err
-		}
-
-		_, err := p.UpdateEndpointImplementation(ctx, &alertingv1alpha.CreateImplementation{
-			NotificationId: &corev1.Reference{Id: *req.UpdateAlert.NotificationId},
-			ConditionId:    &corev1.Reference{Id: req.Id.Id},
-			Implementation: req.UpdateAlert.Details,
-		})
-		if err != nil {
-			return nil, err
-		}
+	if err := handleUpdateEndpointImplementation(p, ctx, req.Id.Id, existing, req.UpdateAlert); err != nil {
+		return nil, err
 	}
-
-	// TODO same as above
-
-	// if same type, naive update
-
-	// if different type, need to delete the old condition checker and create a new one
-
-	switch req.UpdateAlert.AlertType {
-	case &alertingv1alpha.AlertCondition_System{}: // opni system evaluates these conditions elsewhere in the code
-		//
-	default:
-		//
+	// UPDATE THE ACTUAL CONDITION
+	// until we have a more complicated setup, deleting then recreating is fine
+	p.DeleteAlertCondition(ctx, &corev1.Reference{Id: req.Id.Id})
+	_, err = setupCondition(ctx, req.UpdateAlert, req.Id.Id)
+	if err != nil {
+		return nil, err
 	}
 
 	proto.Merge(existing, req.UpdateAlert)
@@ -114,13 +142,22 @@ func (p *Plugin) UpdateAlertCondition(ctx context.Context, req *alertingv1alpha.
 }
 
 func (p *Plugin) DeleteAlertCondition(ctx context.Context, ref *corev1.Reference) (*emptypb.Empty, error) {
-	if err := p.storage.Get().AlertEndpoint.Delete(ctx, path.Join(endpointPrefix, ref.Id)); err != nil {
+	existing, err := p.storage.Get().Conditions.Get(ctx, path.Join(conditionPrefix, ref.Id))
+	if err != nil {
 		return nil, err
 	}
-	return nil, shared.AlertingErrNotImplemented
+	if err := deleteCondition(ctx, existing, ref.Id); err != nil {
+		return nil, err
+	}
+	_, err = p.DeleteEndpointImplementation(ctx, ref)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
 }
 
-func (p *Plugin) PreviewAlertCondition(ctx context.Context, req *alertingv1alpha.PreviewAlertConditionRequest) (*alertingv1alpha.PreviewAlertConditionResponse, error) {
+func (p *Plugin) PreviewAlertCondition(ctx context.Context,
+	req *alertingv1alpha.PreviewAlertConditionRequest) (*alertingv1alpha.PreviewAlertConditionResponse, error) {
 	// Create alert condition
 
 	// measure status

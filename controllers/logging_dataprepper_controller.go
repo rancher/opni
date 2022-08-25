e90433e3f12d7686ad9d46772068c5b2667f2429
Dan Bason <colonel.dan@gmail.com>
2022-08-25 14:26:58 +1200

    Dataprepper reconciler for multiple groups

--- controllers/logging_dataprepper_controller.go
+++ controllers/logging_dataprepper_controller.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"context"
 
-	"github.com/rancher/opni/apis/v1beta2"
+	loggingv1beta1 "github.com/rancher/opni/apis/logging/v1beta1"
 	"github.com/rancher/opni/pkg/resources"
 	"github.com/rancher/opni/pkg/resources/dataprepper"
 	"github.com/rancher/opni/pkg/util"
@@ -14,17 +14,17 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type DataPrepperReconciler struct {
+type LoggingDataPrepperReconciler struct {
 	client.Client
 	scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=opni.io,resources=datapreppers,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=opni.io,resources=datapreppers/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=opni.io,resources=datapreppers/finalizers,verbs=update
+// +kubebuilder:rbac:groups=logging.opni.io,resources=datapreppers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=logging.opni.io,resources=datapreppers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=logging.opni.io,resources=datapreppers/finalizers,verbs=update
 
-func (r *DataPrepperReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	DataPrepper := &v1beta2.DataPrepper{}
+func (r *LoggingDataPrepperReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	DataPrepper := &loggingv1beta1.DataPrepper{}
 	err := r.Get(ctx, req.NamespacedName, DataPrepper)
 	if err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
@@ -50,11 +50,11 @@ func (r *DataPrepperReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *DataPrepperReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *LoggingDataPrepperReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	r.Client = mgr.GetClient()
 	r.scheme = mgr.GetScheme()
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&v1beta2.DataPrepper{}).
+		For(&loggingv1beta1.DataPrepper{}).
 		Owns(&corev1.Secret{}).
 		Owns(&corev1.Service{}).
 		Owns(&appsv1.Deployment{}).

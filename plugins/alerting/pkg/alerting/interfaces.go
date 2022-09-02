690093b5cb0e9f6bb549dd89d69916af1901d496
Alexandre Lamarre <alexandre.lamarre@suse.com>
2022-09-02 10:13:04 -0400

    Template for modular condition backends

--- plugins/alerting/pkg/alerting/interfaces.go
+++ plugins/alerting/pkg/alerting/interfaces.go
@@ -2,10 +2,15 @@ package alerting
 
 import (
 	"context"
+	alertingv1alpha "github.com/rancher/opni/pkg/apis/alerting/v1alpha"
+	corev1 "github.com/rancher/opni/pkg/apis/core/v1"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"sync"
 )
+
+var AlertingBackends = map[alertingv1alpha.AlertType]AlertTypeBackend{}
 
 var mu sync.Mutex
 var datasourceToAlertingCapability map[string]AlertingStore = make(map[string]AlertingStore)
@@ -23,6 +28,23 @@ func UnregisterDatasource(datasource string) {
 }
 
 type AlertingStore interface {
+}
+
+type AlertTypeBackend interface {
+	// ListTemplates List templates available for this alert type
+	ListTemplates() []TemplateInfo
+	// ListChoices List choices for values to be filled in
+	ListChoices() error
+	Create(p *Plugin, req alertingv1alpha.AlertCondition) (*corev1.Reference, error)
+	Update(p *Plugin, req alertingv1alpha.AlertConditionWithId) (*emptypb.Empty, error)
+	Delete(p *Plugin, req *corev1.Reference) (*emptypb.Empty, error)
+
+	WithCurrentRequest(req proto.Message, ctx context.Context) AlertTypeBackend
+}
+
+type TemplateInfo struct {
+	Name        string
+	Description string
 }
 
 type RequestBase struct {
@@ -45,4 +67,38 @@ func NewAlertingMonitoringStore(p *Plugin, lg *zap.SugaredLogger) *AlertingMonit
 			lg:  lg,
 		},
 	}
+}
+
+func NewAlertTypeSystemBackend(p *Plugin, lg *zap.SugaredLogger) AlertTypeBackend {
+	return &AlertTypeSystemBackend{
+		RequestBase{
+			req: nil,
+			p:   p,
+			ctx: context.Background(),
+			lg:  lg,
+		},
+	}
+}
+
+func (a *AlertTypeSystemBackend) WithCurrentRequest(req proto.Message, ctx context.Context) AlertTypeBackend {
+	a.req = req
+	a.ctx = ctx
+	return a
+}
+
+func NewAlertTypeKubeMetricsBackend(p *Plugin, lg *zap.SugaredLogger) AlertTypeBackend {
+	return &AlertTypeKubeMetricsBackend{
+		RequestBase{
+			req: nil,
+			p:   p,
+			ctx: context.Background(),
+			lg:  lg,
+		},
+	}
+}
+
+func (a *AlertTypeKubeMetricsBackend) WithCurrentRequest(req proto.Message, ctx context.Context) AlertTypeBackend {
+	a.req = req
+	a.ctx = ctx
+	return a
 }

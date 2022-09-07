3a4a780b2cfa9bb1acbc9e63beb4e19f8b44e465
Alexandre Lamarre <alexandre.lamarre@suse.com>
2022-09-07 18:07:55 -0400

    more validation & allow http body posting to grpc

--- plugins/slo/pkg/apis/slo/validate.go
+++ plugins/slo/pkg/apis/slo/validate.go
@@ -24,6 +24,9 @@ func (slo *ServiceLevelObjective) Validate() error {
 	if slo.Datasource != shared.MonitoringDatasource && slo.Datasource != shared.LoggingDatasource {
 		return shared.ErrInvalidDatasource
 	}
+	if slo.GetName() == "" {
+		return validation.Error("slo name must be set")
+	}
 	if slo.GetServiceId() == "" {
 		return validation.Error("service must be set")
 	}
@@ -36,13 +39,16 @@ func (slo *ServiceLevelObjective) Validate() error {
 	if slo.GetClusterId() == "" {
 		return validation.Error("clusterId must be set")
 	}
+	if slo.GetGoodEvents() == nil {
+		return validation.Error("goodEvents must always be set for creating an SLO")
+	}
 	for _, v := range slo.GetGoodEvents() {
 		if v.GetKey() == "" {
 			return validation.Error("If an event is provided, its key must be set")
-			for _, vals := range v.GetVals() {
-				if vals == "" {
-					return validation.Error("If an event is provided, one of its values cannot be empty")
-				}
+		}
+		for _, vals := range v.GetVals() {
+			if vals == "" {
+				return validation.Error("If an event is provided, one of its values cannot be empty")
 			}
 		}
 	}
@@ -55,10 +61,17 @@ func (slo *ServiceLevelObjective) Validate() error {
 
 func (c *CreateSLORequest) Validate() error {
 	slo := c.GetSlo()
+	if slo == nil {
+		return validation.Error("creating SLO : slo attribute must be set")
+	}
 	return slo.Validate()
 }
 
 func (s *SLOData) Validate() error {
+	slo := s.GetSLO()
+	if slo == nil {
+		return validation.Error("SLO data : slo attribute must be set")
+	}
 	return s.GetSLO().Validate()
 }
 

30a935825ae1f2ba691d8cf6848860fd3e727de4
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-10-03 16:22:53 -0700

    Update proto files to reflect workload log count

--- plugins/model_training/pkg/model_training/api.go
+++ plugins/model_training/pkg/model_training/api.go
@@ -2,17 +2,22 @@ package model_training
 
 import (
 	"context"
+	"encoding/json"
 
+	corev1 "github.com/rancher/opni/pkg/apis/core/v1"
 	model_training "github.com/rancher/opni/plugins/model_training/pkg/apis/model_training"
 )
 
-func (c *ModelTrainingPlugin) ListWorkloads(ctx context.Context, in *model_training.ClusterID) (*model_training.WorkloadResponse, error) {
-	/*
-		tester, _ := js.KeyValue("os-workload-aggregation")
-		fmt.Println(tester)
-		fmt.Println(tester.Keys())
-		result, _ := kv.Get("aggregation")
-		jsonRes := result.Value()
-	*/
-	return nil, nil
+func (c *ModelTrainingPlugin) WorkloadLogCount(ctx context.Context, in *corev1.Reference) (*model_training.WorkloadsList, error) {
+	result, _ := c.kv.Get("aggregation")
+	jsonRes := result.Value()
+	var results_storage = map[string]map[string]map[string]int{}
+	if err := json.Unmarshal(jsonRes, &results_storage); err != nil {
+		panic(err)
+	}
+	cluster_aggregation_results := results_storage[in.Id]
+	json_aggregation_results, _ := json.Marshal(cluster_aggregation_results)
+	workload_response := *&model_training.WorkloadResponse{}
+	workload_response.Message = string(json_aggregation_results)
+	return &workload_response, nil
 }
--- plugins/model_training/pkg/model_training/plugin.go
+++ plugins/model_training/pkg/model_training/plugin.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	backoffv2 "github.com/lestrrat-go/backoff/v2"
 	"github.com/nats-io/nats.go"
 	managementv1 "github.com/rancher/opni/pkg/apis/management/v1"
 	"github.com/rancher/opni/pkg/logger"
@@ -17,6 +18,7 @@ import (
 
 type ModelTrainingPlugin struct {
 	system.UnimplementedSystemPluginClient
+	ctx            context.Context
 	Logger         *zap.SugaredLogger
 	k8sClient      client.Client
 	natsConnection *nats.Conn
@@ -41,6 +43,23 @@ func (s *ModelTrainingPlugin) UseManagementAPI(api managementv1.ManagementClient
 	}
 	for _, ext := range list.Items {
 		lg.Info("found API extension service", "name", ext.ServiceDesc.GetName())
+	}
+}
+
+func (s *ModelTrainingPlugin) get_kv() {
+	retrier := backoffv2.Exponential(
+		backoffv2.WithMaxRetries(0),
+		backoffv2.WithMinInterval(5*time.Second),
+		backoffv2.WithMaxInterval(1*time.Minute),
+		backoffv2.WithMultiplier(1.1),
+	)
+	b := retrier.Start(s.ctx)
+	for backoffv2.Continue(b) {
+		nc, err := s.newNatsConnection()
+		if err == nil {
+			break
+		}
+		s.Logger.Error("failed to connect to nats, retrying")
 	}
 }
 

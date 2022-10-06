5004ad784eb0bd10738b574ae3c1fda4d57d4d31
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-10-06 11:06:44 -0700

    Update grpc proto file for model training

--- plugins/model_training/pkg/apis/model_training/model_training_grpc.pb.go
+++ plugins/model_training/pkg/apis/model_training/model_training_grpc.pb.go
@@ -12,6 +12,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -24,6 +25,8 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ModelTrainingClient interface {
 	WorkloadLogCount(ctx context.Context, in *v1.Reference, opts ...grpc.CallOption) (*WorkloadsList, error)
+	ModelStatus(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*v1.Reference, error)
+	ModelTrainingParameters(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*WorkloadsList, error)
 }
 
 type modelTrainingClient struct {
@@ -43,11 +46,31 @@ func (c *modelTrainingClient) WorkloadLogCount(ctx context.Context, in *v1.Refer
 	return out, nil
 }
 
+func (c *modelTrainingClient) ModelStatus(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*v1.Reference, error) {
+	out := new(v1.Reference)
+	err := c.cc.Invoke(ctx, "/model_training.ModelTraining/ModelStatus", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *modelTrainingClient) ModelTrainingParameters(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*WorkloadsList, error) {
+	out := new(WorkloadsList)
+	err := c.cc.Invoke(ctx, "/model_training.ModelTraining/ModelTrainingParameters", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ModelTrainingServer is the server API for ModelTraining service.
 // All implementations must embed UnimplementedModelTrainingServer
 // for forward compatibility
 type ModelTrainingServer interface {
 	WorkloadLogCount(context.Context, *v1.Reference) (*WorkloadsList, error)
+	ModelStatus(context.Context, *emptypb.Empty) (*v1.Reference, error)
+	ModelTrainingParameters(context.Context, *emptypb.Empty) (*WorkloadsList, error)
 	mustEmbedUnimplementedModelTrainingServer()
 }
 
@@ -57,6 +80,12 @@ type UnimplementedModelTrainingServer struct {
 
 func (UnimplementedModelTrainingServer) WorkloadLogCount(context.Context, *v1.Reference) (*WorkloadsList, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method WorkloadLogCount not implemented")
+}
+func (UnimplementedModelTrainingServer) ModelStatus(context.Context, *emptypb.Empty) (*v1.Reference, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ModelStatus not implemented")
+}
+func (UnimplementedModelTrainingServer) ModelTrainingParameters(context.Context, *emptypb.Empty) (*WorkloadsList, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ModelTrainingParameters not implemented")
 }
 func (UnimplementedModelTrainingServer) mustEmbedUnimplementedModelTrainingServer() {}
 
@@ -89,6 +118,42 @@ func _ModelTraining_WorkloadLogCount_Handler(srv interface{}, ctx context.Contex
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ModelTraining_ModelStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ModelTrainingServer).ModelStatus(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/model_training.ModelTraining/ModelStatus",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ModelTrainingServer).ModelStatus(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ModelTraining_ModelTrainingParameters_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ModelTrainingServer).ModelTrainingParameters(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/model_training.ModelTraining/ModelTrainingParameters",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ModelTrainingServer).ModelTrainingParameters(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ModelTraining_ServiceDesc is the grpc.ServiceDesc for ModelTraining service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -99,6 +164,14 @@ var ModelTraining_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "WorkloadLogCount",
 			Handler:    _ModelTraining_WorkloadLogCount_Handler,
+		},
+		{
+			MethodName: "ModelStatus",
+			Handler:    _ModelTraining_ModelStatus_Handler,
+		},
+		{
+			MethodName: "ModelTrainingParameters",
+			Handler:    _ModelTraining_ModelTrainingParameters_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},

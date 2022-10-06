d4e19ce1441a1af0771e2ae0579855d2e7cddb57
Alexandre Lamarre <alexandre.lamarre@suse.com>
2022-10-06 18:04:30 -0400

    topology remote write client

--- plugins/topology/pkg/topology/agent/plugin.go
+++ plugins/topology/pkg/topology/agent/plugin.go
@@ -6,11 +6,9 @@ import (
 	healthpkg "github.com/rancher/opni/pkg/health"
 	"github.com/rancher/opni/pkg/logger"
 	"github.com/rancher/opni/pkg/plugins/apis/apiextensions/stream"
-	"github.com/rancher/opni/pkg/plugins/apis/system"
 	"github.com/rancher/opni/pkg/plugins/meta"
 	"github.com/rancher/opni/pkg/util/future"
 	"github.com/rancher/opni/plugins/topology/pkg/apis/node"
-	"github.com/rancher/opni/plugins/topology/pkg/apis/remote"
 	"github.com/rancher/opni/plugins/topology/pkg/topology/agent/drivers"
 	"go.uber.org/zap"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -20,12 +18,10 @@ type Plugin struct {
 	ctx    context.Context
 	logger *zap.SugaredLogger
 
-	remote.UnsafeRemoteTopologyServer
 	node             *TopologyNode
 	topologyStreamer *TopologyStreamer
 
-	system.UnimplementedSystemPluginClient // FIXME: maybe don't need this
-	k8sClient                              future.Future[client.Client]
+	k8sClient future.Future[client.Client]
 
 	stopStreaming context.CancelFunc
 }
@@ -34,9 +30,10 @@ func NewPlugin(ctx context.Context) *Plugin {
 	lg := logger.NewPluginLogger().Named("topology")
 	ct := healthpkg.NewDefaultConditionTracker(lg)
 	p := &Plugin{
-		ctx:    ctx,
-		logger: lg,
-		node:   NewTopologyNode(ct, lg),
+		ctx:       ctx,
+		logger:    lg,
+		node:      NewTopologyNode(ct, lg),
+		k8sClient: future.New[client.Client](),
 	}
 
 	if d, err := drivers.NewExternalTopologyOperatorDriver(lg.Named("external-topology-operator")); err != nil {
@@ -93,12 +90,10 @@ func (p *Plugin) onConfigUpdated(cfg *node.TopologyCapabilityConfig) {
 	}
 }
 
-var _ remote.RemoteTopologyServer = (*Plugin)(nil)
-
 func Scheme(ctx context.Context) meta.Scheme {
 	scheme := meta.NewScheme(meta.WithMode(meta.ModeAgent))
 	p := NewPlugin(ctx)
-	scheme.Add(system.SystemPluginID, system.NewPlugin(p))
+	// scheme.Add(system.SystemPluginID, system.NewPlugin(p))
 	scheme.Add(stream.StreamAPIExtensionPluginID, stream.NewPlugin(p))
 	return scheme
 }

ab7456bad8246e079f30a92c10a9f38121eb5d64
Alexandre Lamarre <alexandre.lamarre@suse.com>
2022-10-06 14:53:40 -0400

    topology gateway apis template

--- plugins/topology/pkg/apis/orchestrator/orchestrator_grpc.pb.go
+++ plugins/topology/pkg/apis/orchestrator/orchestrator_grpc.pb.go
@@ -7,7 +7,12 @@
 package orchestrator
 
 import (
+	context "context"
+	v1 "github.com/rancher/opni/pkg/apis/core/v1"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -19,6 +24,8 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TopologyOrchestratorClient interface {
+	Put(ctx context.Context, in *TopologyGraph, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	Get(ctx context.Context, in *v1.Reference, opts ...grpc.CallOption) (*TopologyGraph, error)
 }
 
 type topologyOrchestratorClient struct {
@@ -29,10 +36,30 @@ func NewTopologyOrchestratorClient(cc grpc.ClientConnInterface) TopologyOrchestr
 	return &topologyOrchestratorClient{cc}
 }
 
+func (c *topologyOrchestratorClient) Put(ctx context.Context, in *TopologyGraph, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, "/orchestrator.TopologyOrchestrator/Put", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *topologyOrchestratorClient) Get(ctx context.Context, in *v1.Reference, opts ...grpc.CallOption) (*TopologyGraph, error) {
+	out := new(TopologyGraph)
+	err := c.cc.Invoke(ctx, "/orchestrator.TopologyOrchestrator/Get", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // TopologyOrchestratorServer is the server API for TopologyOrchestrator service.
 // All implementations must embed UnimplementedTopologyOrchestratorServer
 // for forward compatibility
 type TopologyOrchestratorServer interface {
+	Put(context.Context, *TopologyGraph) (*emptypb.Empty, error)
+	Get(context.Context, *v1.Reference) (*TopologyGraph, error)
 	mustEmbedUnimplementedTopologyOrchestratorServer()
 }
 
@@ -40,6 +67,12 @@ type TopologyOrchestratorServer interface {
 type UnimplementedTopologyOrchestratorServer struct {
 }
 
+func (UnimplementedTopologyOrchestratorServer) Put(context.Context, *TopologyGraph) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Put not implemented")
+}
+func (UnimplementedTopologyOrchestratorServer) Get(context.Context, *v1.Reference) (*TopologyGraph, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
+}
 func (UnimplementedTopologyOrchestratorServer) mustEmbedUnimplementedTopologyOrchestratorServer() {}
 
 // UnsafeTopologyOrchestratorServer may be embedded to opt out of forward compatibility for this service.
@@ -53,13 +86,58 @@ func RegisterTopologyOrchestratorServer(s grpc.ServiceRegistrar, srv TopologyOrc
 	s.RegisterService(&TopologyOrchestrator_ServiceDesc, srv)
 }
 
+func _TopologyOrchestrator_Put_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(TopologyGraph)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TopologyOrchestratorServer).Put(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/orchestrator.TopologyOrchestrator/Put",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TopologyOrchestratorServer).Put(ctx, req.(*TopologyGraph))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _TopologyOrchestrator_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(v1.Reference)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TopologyOrchestratorServer).Get(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/orchestrator.TopologyOrchestrator/Get",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TopologyOrchestratorServer).Get(ctx, req.(*v1.Reference))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // TopologyOrchestrator_ServiceDesc is the grpc.ServiceDesc for TopologyOrchestrator service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var TopologyOrchestrator_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "orchestrator.TopologyOrchestrator",
 	HandlerType: (*TopologyOrchestratorServer)(nil),
-	Methods:     []grpc.MethodDesc{},
-	Streams:     []grpc.StreamDesc{},
-	Metadata:    "github.com/rancher/opni/pkg/plugins/topology/pkg/apis/orchestrator/orchestrator.proto",
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Put",
+			Handler:    _TopologyOrchestrator_Put_Handler,
+		},
+		{
+			MethodName: "Get",
+			Handler:    _TopologyOrchestrator_Get_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "github.com/rancher/opni/pkg/plugins/topology/pkg/apis/orchestrator/orchestrator.proto",
 }
--- plugins/topology/pkg/topology/gateway/plugin.go
+++ plugins/topology/pkg/topology/gateway/plugin.go
@@ -3,23 +3,30 @@ package gateway
 import (
 	"context"
 
+	"github.com/nats-io/nats.go"
 	managementv1 "github.com/rancher/opni/pkg/apis/management/v1"
 	"github.com/rancher/opni/pkg/logger"
 	managementext "github.com/rancher/opni/pkg/plugins/apis/apiextensions/management"
 	"github.com/rancher/opni/pkg/plugins/apis/system"
 	"github.com/rancher/opni/pkg/plugins/meta"
+	"github.com/rancher/opni/pkg/storage"
 	"github.com/rancher/opni/pkg/util"
 	"github.com/rancher/opni/pkg/util/future"
 	"github.com/rancher/opni/plugins/topology/pkg/apis/orchestrator"
 	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type Plugin struct {
-	ctx    context.Context
-	logger *zap.SugaredLogger
 	orchestrator.UnsafeTopologyOrchestratorServer
 	system.UnimplementedSystemPluginClient
+
+	ctx    context.Context
+	logger *zap.SugaredLogger
+
+	nc      future.Future[*nats.Conn]
+	storage future.Future[ConfigStorageAPIs]
 
 	mgmtClient future.Future[managementv1.ManagementClient]
 	k8sClient  future.Future[client.Client]
@@ -27,12 +34,21 @@ type Plugin struct {
 
 func NewPlugin(ctx context.Context) *Plugin {
 	return &Plugin{
-		ctx:    ctx,
-		logger: logger.NewPluginLogger().Named("topology"),
+		ctx:        ctx,
+		logger:     logger.NewPluginLogger().Named("topology"),
+		nc:         future.New[*nats.Conn](),
+		storage:    future.New[ConfigStorageAPIs](),
+		mgmtClient: future.New[managementv1.ManagementClient](),
+		k8sClient:  future.New[client.Client](),
 	}
 }
 
 var _ orchestrator.TopologyOrchestratorServer = (*Plugin)(nil)
+
+type ConfigStorageAPIs struct {
+	//FIXME: to rename if we find a use
+	Placeholder storage.KeyValueStoreT[proto.Message]
+}
 
 func Scheme(ctx context.Context) meta.Scheme {
 	scheme := meta.NewScheme(meta.WithMode(meta.ModeGateway))

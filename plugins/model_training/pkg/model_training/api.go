47c38bc10d3f685ac77ccfb6f60e8314c38578f6
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-10-06 11:05:58 -0700

    Update plugin code

--- plugins/model_training/pkg/model_training/api.go
+++ plugins/model_training/pkg/model_training/api.go
@@ -3,13 +3,15 @@ package model_training
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	corev1 "github.com/rancher/opni/pkg/apis/core/v1"
 	model_training "github.com/rancher/opni/plugins/model_training/pkg/apis/model_training"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (c *ModelTrainingPlugin) WorkloadLogCount(ctx context.Context, in *corev1.Reference) (*model_training.WorkloadsList, error) {
-	result, _ := c.kv.Get("aggregation")
+	result, _ := c.kv.Get().Get("aggregation")
 	jsonRes := result.Value()
 	var results_storage = map[string]map[string]map[string]int{}
 	if err := json.Unmarshal(jsonRes, &results_storage); err != nil {
@@ -26,4 +28,42 @@ func (c *ModelTrainingPlugin) WorkloadLogCount(ctx context.Context, in *corev1.R
 	}
 	workloads_list.List = workload_array
 	return &workloads_list, nil
+}
+
+func (c *ModelTrainingPlugin) ModelStatus(ctx context.Context, in *emptypb.Empty) (*corev1.Reference, error) {
+	b := []byte("model_status")
+	msg, err := c.natsConnection.Get().Request("model_status", b, time.Second)
+	if err != nil {
+		return nil, err
+	}
+	res := corev1.Reference{Id: string(msg.Data)}
+	return &res, nil
+}
+
+func (c *ModelTrainingPlugin) ModelTrainingParameters(ctx context.Context, in *emptypb.Empty) (*model_training.WorkloadsList, error) {
+	b := []byte("model_training_parameters")
+	msg, err := c.natsConnection.Get().Request("workload_parameters", b, time.Second)
+	if err != nil {
+		return nil, err
+	}
+	training_parameters := model_training.WorkloadsList{}
+	parameters_array := make([]*model_training.WorkloadResponse, 0)
+	var results_storage = map[string]map[string][]string{}
+	if err := json.Unmarshal(msg.Data, &results_storage); err != nil {
+		return nil, err
+	}
+	for cluster_name, namespaces := range results_storage {
+		for namespace_name, deployments := range namespaces {
+			if deployments == nil {
+				c.Logger.Error("Unexpected nil deployment for array.")
+				continue
+			}
+			for deployment_idx := range deployments {
+				deployment_data := model_training.WorkloadResponse{ClusterId: cluster_name, Namespace: namespace_name, Deployment: deployments[deployment_idx]}
+				parameters_array = append(parameters_array, &deployment_data)
+			}
+		}
+	}
+	training_parameters.List = parameters_array
+	return &training_parameters, nil
 }
--- plugins/model_training/pkg/model_training/plugin.go
+++ plugins/model_training/pkg/model_training/plugin.go
@@ -10,6 +10,7 @@ import (
 	"github.com/rancher/opni/pkg/logger"
 	"github.com/rancher/opni/pkg/plugins/apis/system"
 	"github.com/rancher/opni/pkg/plugins/meta"
+	"github.com/rancher/opni/pkg/util/future"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -20,8 +21,8 @@ type ModelTrainingPlugin struct {
 	ctx            context.Context
 	Logger         *zap.SugaredLogger
 	k8sClient      client.Client
-	natsConnection *nats.Conn
-	kv             nats.KeyValue
+	natsConnection future.Future[*nats.Conn]
+	kv             future.Future[nats.KeyValue]
 }
 
 func (s *ModelTrainingPlugin) UseManagementAPI(api managementv1.ManagementClient) {
@@ -43,17 +44,20 @@ func (s *ModelTrainingPlugin) UseManagementAPI(api managementv1.ManagementClient
 	for _, ext := range list.Items {
 		lg.Info("found API extension service", "name", ext.ServiceDesc.GetName())
 	}
+	nc, _ := newNatsConnection()
+	mgr, _ := nc.JetStream()
+	keyValue, _ := mgr.KeyValue("os-workload-aggregation")
+	s.natsConnection.Set(nc)
+	s.kv.Set(keyValue)
+
 }
 
 func Scheme(ctx context.Context) meta.Scheme {
 	scheme := meta.NewScheme()
-	nc, _ := newNatsConnection()
-	mgr, _ := nc.JetStream()
-	keyValue, _ := mgr.KeyValue("os-workload-aggregation")
 	p := &ModelTrainingPlugin{
 		Logger:         logger.NewPluginLogger().Named("model_training"),
-		natsConnection: nc,
-		kv:             keyValue,
+		natsConnection: future.New[*nats.Conn](),
+		kv:             future.New[nats.KeyValue](),
 	}
 	scheme.Add(system.SystemPluginID, system.NewPlugin(p))
 	return scheme

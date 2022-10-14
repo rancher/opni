39ae227fe9be4a76d62129cfd796daa87ccfa7fc
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-10-14 22:33:24 +0000

    Add opensearch aggregation routine as part of training controller gateway plugin

--- plugins/model_training/pkg/model_training/plugin.go
+++ plugins/model_training/pkg/model_training/plugin.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/nats-io/nats.go"
+	opensearch "github.com/opensearch-project/opensearch-go"
 	managementv1 "github.com/rancher/opni/pkg/apis/management/v1"
 	"github.com/rancher/opni/pkg/logger"
 	managementext "github.com/rancher/opni/pkg/plugins/apis/apiextensions/management"
@@ -26,6 +27,7 @@ type ModelTrainingPlugin struct {
 	ctx            context.Context
 	Logger         *zap.SugaredLogger
 	k8sClient      client.Client
+	osClient       future.Future[*opensearch.Client]
 	natsConnection future.Future[*nats.Conn]
 	kv             future.Future[nats.KeyValue]
 }
@@ -58,17 +60,26 @@ func (s *ModelTrainingPlugin) UseManagementAPI(api managementv1.ManagementClient
 	if err != nil {
 		os.Exit(1)
 	}
+	client, err := newOpensearchConnection()
+	if err != nil {
+		os.Exit(1)
+	}
 	s.natsConnection.Set(nc)
 	s.kv.Set(keyValue)
+	s.osClient.Set(client)
+	go s.run_aggregation()
 
 }
+
 var _ model_training.ModelTrainingServer = (*ModelTrainingPlugin)(nil)
+
 func Scheme(ctx context.Context) meta.Scheme {
 	scheme := meta.NewScheme()
 	p := &ModelTrainingPlugin{
 		Logger:         logger.NewPluginLogger().Named("model_training"),
 		natsConnection: future.New[*nats.Conn](),
 		kv:             future.New[nats.KeyValue](),
+		osClient:       future.New[*opensearch.Client](),
 	}
 	scheme.Add(system.SystemPluginID, system.NewPlugin(p))
 	scheme.Add(managementext.ManagementAPIExtensionPluginID,
--- plugins/model_training/pkg/model_training/system.go
+++ plugins/model_training/pkg/model_training/system.go
@@ -1,11 +1,14 @@
 package model_training
 
 import (
+	"crypto/tls"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/cenkalti/backoff"
 	"github.com/nats-io/nats.go"
+	opensearch "github.com/opensearch-project/opensearch-go"
 )
 
 func newNatsConnection() (*nats.Conn, error) {
@@ -31,4 +34,18 @@ func newNatsConnection() (*nats.Conn, error) {
 			},
 		),
 	)
+}
+
+func newOpensearchConnection() (*opensearch.Client, error) {
+	ES_ENDPOINT := "https://opni-opensearch-svc.opni-cluster-system.svc:9200"
+	ES_USERNAME := "admin"
+	ES_PASSWORD := "admin"
+	return opensearch.NewClient(opensearch.Config{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		Addresses: []string{ES_ENDPOINT},
+		Username:  ES_USERNAME,
+		Password:  ES_PASSWORD,
+	})
 }

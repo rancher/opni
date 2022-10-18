7fcb72dac8eecfc3c17bcbcf0c62d696dce04284
Joe Kralicky <joe.kralicky@suse.com>
2022-10-17 22:40:45 -0400

    remove old signal handling code from pkg/plugins

--- pkg/plugins/core.go
+++ pkg/plugins/core.go
@@ -3,8 +3,6 @@ package plugins
 import (
 	"context"
 	"errors"
-	"os"
-	"os/signal"
 
 	"github.com/hashicorp/go-plugin"
 	"github.com/rancher/opni/pkg/plugins/meta"
@@ -23,17 +21,6 @@ var Handshake = plugin.HandshakeConfig{
 	ProtocolVersion:  plugin.CoreProtocolVersion,
 	MagicCookieKey:   "OPNI_MAGIC_COOKIE",
 	MagicCookieValue: "opni",
-}
-
-func init() {
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt)
-
-	go func() {
-		<-c
-		plugin.CleanupClients()
-		os.Exit(0)
-	}()
 }
 
 func CheckAvailability(ctx context.Context, cc *grpc.ClientConn, id string) error {

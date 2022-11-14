4ab3c11e22a1b26c2ef3c0fa2742040d3ccc9713
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-11-14 14:49:45 -0800

    Training gateway plugin (#718)
    * Introduce gateway plugin for model training
    
    * Update protobuf definitions for WorkloadResponse
    
    * Update proto files to reflect workload log count
    
    * Update model_training proto file as well as api, plugin and system files
    
    * Update modifications to api.go, plugin.go and system.go files
    
    * Update plugin code
    
    * Update grpc proto file for model training
    
    * Update plugin for model training to address bug
    
    * Update plugin
    
    * Update API for training a model
    
    * Update proto files for training controller gateway
    
    * Update proto files for training controller gateway
    
    * Add endpoint for checking GPU
    
    * Update model training api to search for GPU
    
    * Add opensearch aggregation routine as part of training controller gateway plugin
    
    * Update model training plugin to include service for aggregating data from Opensearch
    
    * Rename directory from modelTraining camel case to modeltraining
    
    * Update model training directory names
    
    * Fix up modeltraining plugin imports
    
    * Update main.go file to reflect latest naming style
    
    * Undo changes to opensearch image in repository
    
    * Update plugin to fetch Opensearch endpoint and credentials
    
    * Update code to make sure Opensearch credentials are properly being fetched
    
    * Remove logging debug statements
    
    * Update code to address PR comments
    
    * Remove unused structs
    
    * Update code to follow gofmt
    
    * Update proto to rename GPUInfo list to items
    
    * Update proto files with PR comments
    
    * Update aggregation service to use context from ModelTrainingPlugin
    
    * Update proto file definitions to be more distinct for each endpoint
    
    * Add error handling for api endpoints and update proto definitions
    
    * Update plugin file to check if os-workload-aggregation already exists in Jetstream
    
    * Update proto definitions to be more concise
    
    * Update struct field formatting to make it consistent
    
    * Update composite aggregation size to be from 4 records per scroll to 1000 records per scroll

---
+++ plugins/modeltraining/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rancher/opni/pkg/plugins"
+	"github.com/rancher/opni/pkg/tracing"
+	"github.com/rancher/opni/pkg/util/waitctx"
+	modeltraining "github.com/rancher/opni/plugins/modeltraining/pkg/modeltraining"
+)
+
+func main() {
+	tracing.Configure("plugin_modeltraining")
+	gin.SetMode(gin.ReleaseMode)
+	ctx, ca := context.WithCancel(waitctx.Background())
+	plugins.Serve(modeltraining.Scheme(ctx))
+	ca()
+	waitctx.Wait(ctx, 5*time.Second)
+}
---
+++ plugins/modeltraining/pkg/modeltraining/aggregation.go
@@ -0,0 +1,188 @@
+package modeltraining
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+)
+
+type Key struct {
+	ClusterID      string `json:"cluster_id"`
+	NamespaceName  string `json:"namespace_name"`
+	DeploymentName string `json:"deployment_name"`
+}
+
+type SearchResponse struct {
+	Aggregations AggregationsSpec `json:"aggregations"`
+}
+
+type AggregationsSpec struct {
+	Bucket BucketSpec `json:"bucket"`
+}
+
+type BucketSpec struct {
+	AfterKey *Key     `json:"after_key"`
+	Buckets  []Bucket `json:"buckets"`
+}
+
+type Bucket struct {
+	Key      Key `json:"key"`
+	DocCount int `json:"doc_count"`
+}
+
+type Aggregations struct {
+	ByCluster map[string]*ClusterAggregation `json:",inline,omitEmpty"`
+}
+
+type ClusterAggregation struct {
+	ByNamespace map[string]*NamespaceAggregation `json:",inline,omitEmpty"`
+}
+
+type NamespaceAggregation struct {
+	ByDeployment map[string]*DeploymentLogCount `json:",inline,omitEmpty"`
+}
+
+type DeploymentLogCount struct {
+	DeploymentName string `json:"deployment_name"`
+	Count          int    `json:"doc_count"`
+}
+
+func newAggregations() *Aggregations {
+	return &Aggregations{
+		ByCluster: make(map[string]*ClusterAggregation),
+	}
+}
+
+func (a *Aggregations) Add(bucket Bucket) {
+	if _, ok := a.ByCluster[bucket.Key.ClusterID]; !ok {
+		a.ByCluster[bucket.Key.ClusterID] = &ClusterAggregation{
+			ByNamespace: make(map[string]*NamespaceAggregation),
+		}
+	}
+	cluster := a.ByCluster[bucket.Key.ClusterID]
+	if _, ok := cluster.ByNamespace[bucket.Key.NamespaceName]; !ok {
+		cluster.ByNamespace[bucket.Key.NamespaceName] = &NamespaceAggregation{
+			ByDeployment: make(map[string]*DeploymentLogCount),
+		}
+	}
+	a.ByCluster[bucket.Key.ClusterID].ByNamespace[bucket.Key.NamespaceName].ByDeployment[bucket.Key.DeploymentName] = &DeploymentLogCount{
+		DeploymentName: bucket.Key.DeploymentName,
+		Count:          bucket.DocCount,
+	}
+}
+
+func (s *ModelTrainingPlugin) aggregateWorkloadLogs() {
+	request := map[string]any{
+		"size": 0,
+		"query": map[string]any{
+			"bool": map[string]any{
+				"must": []map[string]any{
+					{
+						"match": map[string]any{
+							"log_type": "workload",
+						},
+					},
+					{
+						"regexp": map[string]any{
+							"kubernetes.namespace_name.keyword": ".+",
+						},
+					},
+					{
+						"regexp": map[string]any{
+							"deployment.keyword": ".+",
+						},
+					},
+				},
+			},
+		},
+		"aggs": map[string]any{
+			"bucket": map[string]any{
+				"composite": map[string]any{
+					"size": 1000,
+					"sources": []map[string]any{
+						{
+							"cluster_id": map[string]any{
+								"terms": map[string]any{
+									"field": "cluster_id",
+								},
+							},
+						},
+						{
+							"namespace_name": map[string]any{
+								"terms": map[string]any{
+									"field": "kubernetes.namespace_name.keyword",
+								},
+							},
+						},
+						{
+							"deployment_name": map[string]any{
+								"terms": map[string]any{
+									"field": "deployment.keyword",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	resultAgg := newAggregations()
+	for {
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(request); err != nil {
+			s.Logger.Errorf("Error: Unable to encode request: %s", err)
+			return
+		}
+		res, err := s.osClient.Get().Search(
+			s.osClient.Get().Search.WithContext(s.ctx),
+			s.osClient.Get().Search.WithIndex("logs"),
+			s.osClient.Get().Search.WithBody(&buf),
+			s.osClient.Get().Search.WithTrackTotalHits(true),
+			s.osClient.Get().Search.WithPretty(),
+		)
+		if err != nil {
+			s.Logger.Errorf("Unable to connect to Opensearch %s", err)
+			return
+		}
+		defer res.Body.Close()
+		if res.IsError() {
+			s.Logger.Errorf("Error: %s", res.String())
+			return
+		}
+		var result SearchResponse
+		if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+			s.Logger.Errorf("Error parsing the response body: %s", err)
+			return
+		}
+		for _, b := range result.Aggregations.Bucket.Buckets {
+			resultAgg.Add(b)
+		}
+		afterKey := result.Aggregations.Bucket.AfterKey
+		if afterKey != nil {
+			((request["aggs"].(map[string]any)["bucket"]).(map[string]any)["composite"]).(map[string]any)["after"] = afterKey
+		} else {
+			break
+		}
+	}
+	aggregatedResults, err := json.Marshal(resultAgg)
+	if err != nil {
+		s.Logger.Errorf("Error: %s", err)
+		return
+	}
+	bytesAggregation := []byte(aggregatedResults)
+	s.kv.Get().Put("aggregation", bytesAggregation)
+	s.Logger.Info("Updated aggregation of deployments to Jetstream.")
+}
+
+func (s *ModelTrainingPlugin) runAggregation() {
+	t := time.NewTicker(30 * time.Second)
+	for {
+		select {
+		case <-t.C:
+			s.aggregateWorkloadLogs()
+		case <-s.ctx.Done():
+			t.Stop()
+			return
+		}
+	}
+}

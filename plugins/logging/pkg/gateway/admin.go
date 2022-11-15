2858218a87e8996f1bdc3c00ab66e7d8786a147d
Dan Bason <colonel.dan@gmail.com>
2022-11-15 14:36:01 +1300

    Chore: update versions for release

--- plugins/logging/pkg/gateway/admin.go
+++ plugins/logging/pkg/gateway/admin.go
@@ -465,7 +465,7 @@ func (p *Plugin) convertProtobufToDashboards(
 	cluster *loggingv1beta1.OpniOpensearch,
 ) opsterv1.DashboardsConfig {
 	var osVersion string
-	version := "0.6.1"
+	version := "0.6.2-rc1"
 	if cluster == nil {
 		if util.Version != "unversioned" {
 			version = strings.TrimPrefix(util.Version, "v")
@@ -491,7 +491,7 @@ func (p *Plugin) convertProtobufToDashboards(
 	}
 
 	if version == "unversioned" {
-		version = "0.6.1"
+		version = "0.6.2-rc1"
 	}
 
 	image := fmt.Sprintf(

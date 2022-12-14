f4fe5224c15f098c4994a096b639ffb1a19eea85
Dan Bason <colonel.dan@gmail.com>
2022-12-14 14:18:57 +1300

    Additional logging v2 validation

--- plugins/logging/pkg/gateway/admin_v2.go
+++ plugins/logging/pkg/gateway/admin_v2.go
@@ -166,6 +166,7 @@ func (m *LoggingManagerV2) CreateOrUpdateOpensearchCluster(ctx context.Context, 
 	if cluster.GetDataNodes() == nil {
 		return &emptypb.Empty{}, errors.ErrMissingDataNode()
 	}
+
 	k8sOpensearchCluster := &loggingv1beta1.OpniOpensearch{}
 
 	go m.opensearchManager.SetClient(m.setOpensearchClient)
@@ -565,6 +566,11 @@ func (m *LoggingManagerV2) generateNodePools(cluster *loggingadmin.OpensearchClu
 		return
 	}
 
+	if lo.FromPtrOr(cluster.DataNodes.Replicas, 1) < 1 {
+		retErr = errors.ErrReplicasZero("data")
+		return
+	}
+
 	initialPool := opsterv1.NodePool{
 		Component: "data",
 		Replicas:  lo.FromPtrOr(cluster.DataNodes.Replicas, 1),
@@ -600,7 +606,7 @@ func (m *LoggingManagerV2) generateNodePools(cluster *loggingadmin.OpensearchClu
 	}
 	if cluster.GetControlplaneNodes() == nil {
 		roles = append(roles, "master")
-		if lo.FromPtrOr(cluster.DataNodes.Replicas, 1)%2 == 0 {
+		if lo.FromPtrOr(cluster.DataNodes.Replicas, 1)%2 == 0 || lo.FromPtrOr(cluster.DataNodes.Replicas, 1) < 3 {
 			extraControlPlanePool = true
 		}
 		if lo.FromPtrOr(cluster.DataNodes.Replicas, 1) > 5 {
@@ -639,8 +645,8 @@ func (m *LoggingManagerV2) generateNodePools(cluster *loggingadmin.OpensearchClu
 		cpPool := opsterv1.NodePool{
 			Component: "quorum",
 			Replicas: func() int32 {
-				if lo.FromPtrOr(cluster.DataNodes.Replicas, 1) == 1 {
-					return 2
+				if lo.FromPtrOr(cluster.DataNodes.Replicas, 1) < 3 {
+					return 3 - lo.FromPtrOr(cluster.DataNodes.Replicas, 1)
 				}
 				return 1
 			}(),
@@ -718,19 +724,25 @@ func generateK8sResources(memoryLimit string, cpu *loggingadmin.CPUResource) (co
 	}
 
 	if cpu != nil {
+		var request, limit resource.Quantity
 		if cpu.Request != "" {
-			request, err := resource.ParseQuantity(cpu.Request)
+			request, err = resource.ParseQuantity(cpu.Request)
 			if err != nil {
 				return corev1.ResourceRequirements{}, "", err
 			}
 			resources.Requests[corev1.ResourceCPU] = request
 		}
 		if cpu.Limit != "" {
-			limit, err := resource.ParseQuantity(cpu.Limit)
+			limit, err = resource.ParseQuantity(cpu.Limit)
 			if err != nil {
 				return corev1.ResourceRequirements{}, "", err
 			}
 			resources.Limits[corev1.ResourceCPU] = limit
+		}
+		if cpu.Request != "" && cpu.Limit != "" {
+			if request.Cmp(limit) > 0 {
+				return corev1.ResourceRequirements{}, "", errors.ErrRequestGtLimits()
+			}
 		}
 	}
 	jvm := fmt.Sprintf("-Xmx%d -Xms%d", jvmVal, jvmVal)
@@ -797,6 +809,10 @@ func (m *LoggingManagerV2) convertIngestDetails(details *loggingadmin.IngestDeta
 		return opsterv1.NodePool{}, err
 	}
 
+	if lo.FromPtrOr(details.Replicas, 1) < 1 {
+		return opsterv1.NodePool{}, errors.ErrReplicasZero("ingest")
+	}
+
 	return opsterv1.NodePool{
 		Component: "ingest",
 		Roles: []string{
@@ -837,6 +853,10 @@ func (m *LoggingManagerV2) convertControlplaneDetails(details *loggingadmin.Cont
 	})
 	if err != nil {
 		return opsterv1.NodePool{}, err
+	}
+
+	if lo.FromPtrOr(details.Replicas, 1) < 1 {
+		return opsterv1.NodePool{}, errors.ErrReplicasZero("controlplane")
 	}
 
 	// Give the controlplane nodes slightly more memory that double the jvm

c4f58674e2bd1d7aa999f2a3e4738a6c1d12bdf2
Dan Bason <colonel.dan@gmail.com>
2023-02-10 14:06:12 +1300

    Add collector CRD

---
+++ apis/core/v1beta1/collector_types.go
@@ -0,0 +1,45 @@
+package v1beta1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type CollectorSpec struct {
+	AgentEndpoint string                       `json:"agentEndpoint,omitempty"`
+	LoggingConfig *corev1.LocalObjectReference `json:"loggingConfig,omitempty"`
+}
+
+// CollectorStatus defines the observed state of Collector
+type CollectorStatus struct {
+	Phase      string   `json:"phase,omitempty"`
+	Message    string   `json:"message,omitempty"`
+	Conditions []string `json:"conditions,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+// +kubebuilder:storageversion
+
+// Collector is the Schema for the logadapters API
+type Collector struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CollectorSpec   `json:"spec,omitempty"`
+	Status CollectorStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// CollectorList contains a list of Collector
+type CollectorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Collector `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Collector{}, &CollectorList{})
+}

f66c7d907f76a3f2fec8a66cd92f70c25249c1ca
Alexandre Lamarre <alexandre.lamarre@suse.com>
2023-03-24 11:50:35 -0400

    testsuite for compatibility with other interceptors

--- pkg/util/grpc_test.go
+++ pkg/util/grpc_test.go
@@ -3,6 +3,7 @@ package util_test
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"net"
 	"sync"
 	"time"
@@ -15,6 +16,7 @@ import (
 	"github.com/rancher/opni/pkg/test/testgrpc"
 	"github.com/rancher/opni/pkg/util"
 	"github.com/samber/lo"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/keepalive"
@@ -155,7 +157,7 @@ func BuildCachingInterceptorSuite(
 		testgrpc.ObjectServiceClient,
 		testgrpc.AggregatorServiceClient),
 ) bool {
-	return Describe(fmt.Sprintf("GRPC caching interceptor for %s", name), Ordered, Label("unit"), func() {
+	return Describe(fmt.Sprintf("GRPC caching interceptor for %s", name), Ordered, Label("integration"), func() {
 		var testSimpleServer testgrpc.SimpleServiceServer
 		var testAggregatorServer testgrpc.AggregatorServiceServer
 		var testSimpleClient testgrpc.SimpleServiceClient
@@ -450,6 +452,182 @@ func BuildCachingInterceptorSuite(
 				Eventually(func() int64 {
 					return util.Must(testAggregatorClient.GetAllWithNestedCaching(ctx, &emptypb.Empty{})).Value
 				}, defaultTtl*2, defaultEvictionInterval).Should(Equal(int64(35)))
+			})
+		})
+
+		When("The caching interceptor is used with other interceptors", func() {
+			Specify("ordering should not affect the caching mechanism", func() {
+				// fuzzy ordering
+
+				type s struct {
+					grpc.UnaryServerInterceptor
+					grpc.UnaryClientInterceptor
+				}
+				randomInterceptors := []s{
+					{
+						UnaryServerInterceptor: otelgrpc.UnaryServerInterceptor(),
+						UnaryClientInterceptor: otelgrpc.UnaryClientInterceptor(),
+					},
+				}
+
+				for i := 0; i < 10; i++ {
+					randomInterceptors = append(randomInterceptors, s{
+						UnaryServerInterceptor: func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+							resp, err := handler(ctx, req)
+							clientMd, ok := metadata.FromOutgoingContext(ctx)
+							if ok {
+								clientMd.Append(uuid.New().String(), uuid.New().String())
+								grpc.SetHeader(ctx, clientMd)
+							} else {
+								grpc.SetHeader(ctx, metadata.Pairs(uuid.New().String(), uuid.New().String()))
+							}
+							return resp, err
+						},
+						UnaryClientInterceptor: func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+							var requestMd metadata.MD
+							if err := invoker(ctx, method, req, reply, cc,
+								append(opts,
+									grpc.Header(&requestMd),
+								)...,
+							); err != nil {
+								return err
+							}
+							if requestMd != nil {
+								requestMd.Append(uuid.New().String(), uuid.New().String())
+								grpc.SetHeader(ctx, requestMd)
+							}
+
+							return nil
+						},
+					})
+				}
+
+				numTestcases := 5
+				var mu sync.Mutex
+
+				var wg sync.WaitGroup
+				for i := 0; i < numTestcases; i++ {
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						aggregate := 1
+						By("setting up a cache server")
+						testCacher := buildCache()
+						n := rand.Intn(len(randomInterceptors))
+						registerInterceptors := lo.Samples(randomInterceptors, n)
+						registerInterceptors = append(registerInterceptors, s{
+							UnaryServerInterceptor: testCacher.UnaryServerInterceptor(),
+							UnaryClientInterceptor: testCacher.UnaryClientInterceptor(),
+						})
+						registerInterceptors = lo.Shuffle(registerInterceptors)
+						// append the value to the end of the slice
+						registerInterceptors = append(registerInterceptors, s{
+							UnaryServerInterceptor: testCacher.UnaryServerInterceptor(),
+							UnaryClientInterceptor: testCacher.UnaryClientInterceptor(),
+						})
+
+						// select random index
+						randomIndex := rand.Intn(len(registerInterceptors))
+
+						// shift the elements to the right from the random index to the end of the slice
+						copy(registerInterceptors[randomIndex+1:], registerInterceptors[randomIndex:len(registerInterceptors)-1])
+
+						// serve + client
+
+						// ------ setup standalone servers
+						simpleServer := testgrpc.NewSimpleServer(defaultTtl)
+
+						defaultListener, err := net.Listen("tcp4", fmt.Sprintf("127.0.0.1:%d", freeport.GetFreePort()))
+						Expect(err).To(Succeed())
+
+						server := grpc.NewServer(
+							grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+								MinTime:             15 * time.Second,
+								PermitWithoutStream: true,
+							}),
+							grpc.KeepaliveParams(keepalive.ServerParameters{
+								Time:    15 * time.Second,
+								Timeout: 5 * time.Second,
+							}),
+							grpc.ChainUnaryInterceptor(
+								lo.Map(registerInterceptors, func(item s, _ int) grpc.UnaryServerInterceptor {
+									return item.UnaryServerInterceptor
+								})...,
+							),
+						)
+
+						testgrpc.RegisterSimpleServiceServer(server, simpleServer)
+
+						_ = lo.Async(func() error {
+							return server.Serve(defaultListener)
+						})
+						mu.Lock()
+						DeferCleanup(server.Stop)
+						mu.Unlock()
+						opts := []grpc.DialOption{
+							grpc.WithTransportCredentials(insecure.NewCredentials()),
+							grpc.WithChainUnaryInterceptor(
+								lo.Map(registerInterceptors, func(item s, _ int) grpc.UnaryClientInterceptor {
+									return item.UnaryClientInterceptor
+								})...,
+							),
+						}
+
+						userConn, err := grpc.Dial(defaultListener.Addr().String(), opts...)
+						Expect(err).To(Succeed())
+						mu.Lock()
+						DeferCleanup(userConn.Close)
+						mu.Unlock()
+
+						Expect(err).To(Succeed())
+						simpleClientUser := testgrpc.NewSimpleServiceClient(userConn)
+
+						_, err = simpleClientUser.Increment(ctx, &testgrpc.IncrementRequest{
+							Value: 1,
+						})
+						Expect(err).To(Succeed())
+						ctxMetadata := util.WithGrpcClientCaching(ctx, 5*time.Second)
+
+						md, ok := metadata.FromOutgoingContext(ctxMetadata)
+						Expect(ok).To(BeTrue())
+						Expect(md.Get(util.GrpcCacheControlHeader(util.CacheTypeClient))).NotTo(HaveLen(0))
+
+						value, err := simpleClientUser.GetValue(
+							ctxMetadata,
+							&emptypb.Empty{},
+						)
+						Expect(err).To(Succeed())
+						Expect(value.Value).To(Equal(int64(aggregate)))
+
+						var wg sync.WaitGroup
+						for i := 0; i < 5; i++ {
+							wg.Add(1)
+							go func() {
+								defer wg.Done()
+								_, err := simpleClientUser.Increment(ctx, &testgrpc.IncrementRequest{
+									Value: 1,
+								})
+								Expect(err).To(Succeed())
+							}()
+							aggregate++
+						}
+						wg.Wait()
+						By("verifying the aggregated value is in the server")
+						value, err = simpleServer.GetValue(ctx, &emptypb.Empty{})
+						Expect(err).To(Succeed())
+						Expect(value.Value).To(Equal(int64(aggregate)))
+
+						By("verifying the value in the cache hasn't expired")
+						value, err = simpleClientUser.GetValue(
+							ctx,
+							&emptypb.Empty{},
+						)
+						Expect(err).To(Succeed())
+						Expect(value.Value).To(Equal(int64(1)))
+
+					}()
+				}
+				wg.Wait()
 			})
 		})
 	})

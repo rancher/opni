dbf410a6a20cfeba69a8373e3f2d7b03cf665f07
Joe Kralicky <joe.kralicky@suse.com>
2023-04-06 14:47:17 -0400

    Update node config tests

--- test/plugins/metrics/node_config_test.go
+++ test/plugins/metrics/node_config_test.go
@@ -2,6 +2,7 @@ package metrics_test
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -12,13 +13,16 @@ import (
 	"github.com/rancher/opni/pkg/test/testutil"
 	"github.com/rancher/opni/plugins/metrics/pkg/apis/node"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 var _ = Describe("Node Config", Ordered, Label("integration"), func() {
+	var mgmtClient managementv1.ManagementClient
 	var nodeClient node.NodeConfigurationClient
 	BeforeAll(func() {
 		env := &test.Environment{
@@ -26,7 +30,7 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 		}
 		Expect(env.Start(test.WithEnableCortexClusterDriver(true))).To(Succeed())
 
-		mgmtClient := env.NewManagementClient()
+		mgmtClient = env.NewManagementClient()
 
 		token, err := mgmtClient.CreateBootstrapToken(context.Background(), &managementv1.CreateBootstrapTokenRequest{
 			Ttl: durationpb.New(time.Hour),
@@ -61,16 +65,74 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 		return spec, isDefault, nil
 	}
 
+	var verifySync = func(fn func(), capability string, agentIds ...string) {
+		times := make(map[string]time.Time)
+		for _, id := range agentIds {
+			id := strings.TrimPrefix(id, "!")
+			req := &managementv1.CapabilityStatusRequest{
+				Name:    capability,
+				Cluster: &v1.Reference{Id: id},
+			}
+			Eventually(func() bool {
+				stat, err := mgmtClient.CapabilityStatus(context.Background(), req)
+				if status.Code(err) == codes.NotFound {
+					return false
+				} else {
+					Expect(err).NotTo(HaveOccurred())
+					times[id] = stat.GetLastSync().AsTime()
+				}
+				return true
+			}).Should(BeTrue(), "failed to get sync time for %s", id)
+		}
+
+		fn()
+
+		for _, id := range agentIds {
+			expectNoUpdate := strings.HasPrefix(id, "!")
+			id := strings.TrimPrefix(id, "!")
+			req := &managementv1.CapabilityStatusRequest{
+				Name:    capability,
+				Cluster: &v1.Reference{Id: id},
+			}
+
+			if expectNoUpdate {
+				Consistently(func() bool {
+					stat, err := mgmtClient.CapabilityStatus(context.Background(), req)
+					if status.Code(err) == codes.NotFound {
+						return false
+					} else {
+						Expect(err).NotTo(HaveOccurred())
+					}
+					return stat.GetLastSync().AsTime().Equal(times[id])
+				}).Should(BeTrue(), "expected sync time not to be updated for agent %s", id)
+				continue
+			} else {
+				Eventually(func() bool {
+					stat, err := mgmtClient.CapabilityStatus(context.Background(), req)
+					if status.Code(err) == codes.NotFound {
+						return false
+					} else {
+						Expect(err).NotTo(HaveOccurred())
+					}
+					syncTime := times[id]
+					return stat.GetLastSync().AsTime().After(syncTime)
+				}).Should(BeTrue(), "expected sync time to be updated for agent %s", id)
+			}
+		}
+	}
+
 	var originalDefaultConfig *node.MetricsCapabilitySpec
 	It("should initially have all nodes using the default config", func() {
 		defaultConfig, err := nodeClient.GetDefaultConfiguration(context.Background(), &emptypb.Empty{})
 		Expect(err).NotTo(HaveOccurred())
 
 		spec, isDefault, err := getConfig("agent1")
+		Expect(err).NotTo(HaveOccurred())
 		Expect(spec).To(testutil.ProtoEqual(defaultConfig))
 		Expect(isDefault).To(BeTrue())
 
 		spec, isDefault, err = getConfig("agent2")
+		Expect(err).NotTo(HaveOccurred())
 		Expect(spec).To(testutil.ProtoEqual(defaultConfig))
 		Expect(isDefault).To(BeTrue())
 
@@ -84,14 +146,20 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 					Image: "foo",
 				},
 			}
-			_, err := nodeClient.SetDefaultConfiguration(context.Background(), newConfig)
-			Expect(err).NotTo(HaveOccurred())
+
+			verifySync(func() {
+				_, err := nodeClient.SetDefaultConfiguration(context.Background(), newConfig)
+				Expect(err).NotTo(HaveOccurred())
+			}, "metrics", "agent1", "agent2")
 
 			spec, isDefault, err := getConfig("agent1")
+			Expect(err).NotTo(HaveOccurred())
+
 			Expect(spec).To(testutil.ProtoEqual(newConfig))
 			Expect(isDefault).To(BeTrue())
 
 			spec, isDefault, err = getConfig("agent2")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(newConfig))
 			Expect(isDefault).To(BeTrue())
 		})
@@ -108,17 +176,21 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 			defaultConfig, err := nodeClient.GetDefaultConfiguration(context.Background(), &emptypb.Empty{})
 			Expect(err).NotTo(HaveOccurred())
 
-			_, err = nodeClient.SetNodeConfiguration(context.Background(), &node.NodeConfigRequest{
-				Node: &v1.Reference{Id: "agent1"},
-				Spec: newConfig,
-			})
-			Expect(err).NotTo(HaveOccurred())
+			verifySync(func() {
+				_, err = nodeClient.SetNodeConfiguration(context.Background(), &node.NodeConfigRequest{
+					Node: &v1.Reference{Id: "agent1"},
+					Spec: newConfig,
+				})
+				Expect(err).NotTo(HaveOccurred())
+			}, "metrics", "agent1", "!agent2")
 
 			spec, isDefault, err := getConfig("agent1")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(newConfig))
 			Expect(isDefault).To(BeFalse())
 
 			spec, isDefault, err = getConfig("agent2")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(defaultConfig))
 			Expect(isDefault).To(BeTrue())
 		})
@@ -129,17 +201,21 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 			defaultConfig, err := nodeClient.GetDefaultConfiguration(context.Background(), &emptypb.Empty{})
 			Expect(err).NotTo(HaveOccurred())
 
-			_, err = nodeClient.SetNodeConfiguration(context.Background(), &node.NodeConfigRequest{
-				Node: &v1.Reference{Id: "agent1"},
-				Spec: nil,
-			})
-			Expect(err).NotTo(HaveOccurred())
+			verifySync(func() {
+				_, err = nodeClient.SetNodeConfiguration(context.Background(), &node.NodeConfigRequest{
+					Node: &v1.Reference{Id: "agent1"},
+					Spec: nil,
+				})
+				Expect(err).NotTo(HaveOccurred())
+			}, "metrics", "agent1", "!agent2")
 
 			spec, isDefault, err := getConfig("agent1")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(defaultConfig))
 			Expect(isDefault).To(BeTrue())
 
 			spec, isDefault, err = getConfig("agent2")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(defaultConfig))
 			Expect(isDefault).To(BeTrue())
 		})
@@ -147,14 +223,18 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 
 	When("resetting the default config", func() {
 		It("should return the original default config for all nodes", func() {
-			_, err := nodeClient.SetDefaultConfiguration(context.Background(), &node.MetricsCapabilitySpec{})
-			Expect(err).NotTo(HaveOccurred())
+			verifySync(func() {
+				_, err := nodeClient.SetDefaultConfiguration(context.Background(), &node.MetricsCapabilitySpec{})
+				Expect(err).NotTo(HaveOccurred())
+			}, "metrics", "agent1", "agent2")
 
 			spec, isDefault, err := getConfig("agent1")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(originalDefaultConfig))
 			Expect(isDefault).To(BeTrue())
 
 			spec, isDefault, err = getConfig("agent2")
+			Expect(err).NotTo(HaveOccurred())
 			Expect(spec).To(testutil.ProtoEqual(originalDefaultConfig))
 			Expect(isDefault).To(BeTrue())
 		})
@@ -165,19 +245,23 @@ var _ = Describe("Node Config", Ordered, Label("integration"), func() {
 			defaultConfig, err := nodeClient.GetDefaultConfiguration(context.Background(), &emptypb.Empty{})
 			Expect(err).NotTo(HaveOccurred())
 
-			_, err = nodeClient.SetNodeConfiguration(context.Background(), &node.NodeConfigRequest{
-				Node: &v1.Reference{Id: "agent1"},
-				Spec: defaultConfig,
-			})
-			Expect(err).NotTo(HaveOccurred())
+			verifySync(func() {
+				_, err = nodeClient.SetNodeConfiguration(context.Background(), &node.NodeConfigRequest{
+					Node: &v1.Reference{Id: "agent1"},
+					Spec: defaultConfig,
+				})
+				Expect(err).NotTo(HaveOccurred())
+			}, "metrics", "agent1", "!agent2")
 
 			newConfig := &node.MetricsCapabilitySpec{
 				Prometheus: &node.PrometheusSpec{
 					Image: "foo",
 				},
 			}
-			_, err = nodeClient.SetDefaultConfiguration(context.Background(), newConfig)
-			Expect(err).NotTo(HaveOccurred())
+			verifySync(func() {
+				_, err = nodeClient.SetDefaultConfiguration(context.Background(), newConfig)
+				Expect(err).NotTo(HaveOccurred())
+			}, "metrics", "agent1", "agent2")
 
 			spec, isDefault, err := getConfig("agent1")
 			Expect(spec).To(testutil.ProtoEqual(defaultConfig))

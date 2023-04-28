085df1d61bce9ad35785e3b6df5e1ae471c53fed
Joe Kralicky <joe.kralicky@suse.com>
2023-04-28 16:19:02 -0400

    Add driver imports to plugins/aiops/main.go

--- plugins/aiops/main.go
+++ plugins/aiops/main.go
@@ -4,6 +4,10 @@ import (
 	"github.com/rancher/opni/pkg/plugins"
 	"github.com/rancher/opni/pkg/plugins/meta"
 	"github.com/rancher/opni/plugins/aiops/pkg/gateway"
+
+	_ "github.com/rancher/opni/plugins/aiops/pkg/features/log_anomaly"
+	_ "github.com/rancher/opni/plugins/aiops/pkg/features/metric_anomaly"
+	_ "github.com/rancher/opni/plugins/aiops/pkg/features/metric_anomaly/drivers/opni_manager"
 )
 
 func main() {

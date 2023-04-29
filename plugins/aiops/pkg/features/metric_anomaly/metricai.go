38b6cac188143b8cc8978b5d75247c941536f95a
Yingbei-Tong <tybalex@gmail.com>
2023-04-28 17:53:53 -0700

    merge manually from metric-ai-plugin branch

--- plugins/aiops/pkg/features/metric_anomaly/metricai.go
+++ plugins/aiops/pkg/features/metric_anomaly/metricai.go
@@ -12,6 +12,7 @@ import (
 	"github.com/nats-io/nats.go"
 	"github.com/prometheus/common/model"
 	"github.com/rancher/opni/pkg/metrics/compat"
+	"github.com/rancher/opni/pkg/validation"
 	"github.com/rancher/opni/plugins/aiops/pkg/apis/metricai"
 	"github.com/rancher/opni/plugins/metrics/pkg/apis/cortexadmin"
 	"golang.org/x/exp/slices"
@@ -26,26 +27,17 @@ const (
 	timeout             = 10 * time.Second
 )
 
-type RequestError struct {
-	StatusCode int
-	Err        error
-}
-
-func (r *RequestError) Error() string {
-	return r.Err.Error()
-}
-
 func (p *MetricAnomaly) CreateGrafanaDashboard(ctx context.Context, jobRunId *metricai.MetricAIId) (*metricai.MetricAIAPIResponse, error) {
 	// dashboardJson is generated in the python service. This function simply create a GrafanaDashboard resource with it and apply it
 	res, err := p.GetJobRunResult(ctx, jobRunId)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to Get JobRunRes for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to Get JobRunRes for metricAI: %s", err)
 	}
 	dashboardJson := res.JobRunResultDetails
 	dashboardName := strings.ToLower(strings.ReplaceAll(res.JobRunId, jobRunDelimiter, ""))
 	err = p.DashboardDriver.CreateDashboard(dashboardNamePrefix+dashboardName, dashboardJson)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Error Creating Dashboard for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Error Creating Dashboard for metricAI: %s", err)
 	}
 
 	return &metricai.MetricAIAPIResponse{SubmittedTime: time.Now().String(), Description: dashboardJson, Status: "Success"}, nil
@@ -55,7 +47,7 @@ func (p *MetricAnomaly) DeleteGrafanaDashboard(ctx context.Context, jobRunId *me
 	// delete the grafanadashboard resource for the given jobrun id
 	res, err := p.GetJobRunResult(ctx, jobRunId)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to Get JobRunRes for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to Get JobRunRes for metricAI: %s", err)
 	}
 	dashboardJson := res.JobRunResultDetails
 	dashboardName := strings.ToLower(strings.ReplaceAll(res.JobRunId, jobRunDelimiter, ""))
@@ -74,17 +66,17 @@ func (p *MetricAnomaly) DeleteGrafanaDashboard(ctx context.Context, jobRunId *me
 // 	url := p.MetricAnomalyServiceURL + "/get_users"
 // 	req, err := http.NewRequestWithContext(ctxca, http.MethodGet, url, nil)
 // 	if err != nil {
-// 		return nil, status.Errorf(codes.Internal, "Failed to form httprequest to ListClusters for metricAI: %v", err)
+// 		return nil, status.Errorf(codes.Internal, "Failed to form httprequest to ListClusters for metricAI: %s", err)
 // 	}
 // 	resp, err := p.httpClient.Do(req)
 // 	if err != nil {
-// 		return nil, status.Errorf(codes.Internal, "Failed to make httprequest to ListClusters for metricAI: %v", err)
+// 		return nil, status.Errorf(codes.Internal, "Failed to make httprequest to ListClusters for metricAI: %s", err)
 // 	}
 // 	defer resp.Body.Close()
 
 // 	var result []string
 // 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-// 		return nil, status.Errorf(codes.Internal, "Failed to unmarshal response of ListClusters for metricAI: %v", err)
+// 		return nil, status.Errorf(codes.Internal, "Failed to unmarshal response of ListClusters for metricAI: %s", err)
 // 	}
 // 	return &metricai.MetricAIIdList{Items: result}, nil
 // }
@@ -98,16 +90,16 @@ func (p *MetricAnomaly) ListNamespaces(ctx context.Context, clusterId *metricai.
 		Query:   "kube_namespace_labels",
 	})
 	if err != nil {
-		return nil, status.Errorf(codes.Unavailable, "Failed to query cortex: %v", err)
+		return nil, status.Errorf(codes.Unavailable, "Failed to query cortex: %s", err)
 	}
 
 	queryResult, err := compat.UnmarshalPrometheusResponse(response.Data)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to unmarshal prometheus response: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to unmarshal prometheus response: %s", err)
 	}
 	vec, err := queryResult.GetVector()
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to unmarshal prometheus response: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to unmarshal prometheus response: %s", err)
 	}
 	list := &metricai.MetricAIIdList{}
 	for _, sample := range *vec {
@@ -123,14 +115,14 @@ func (p *MetricAnomaly) ListJobs(ctx context.Context, _ *emptypb.Empty) (*metric
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIJobKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to ListJobs for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to ListJobs for metricAI: %s", err)
 	}
 	jobs, err := metricAIKeyValue.Keys()
 	if err != nil {
 		if errors.Is(err, nats.ErrNoKeysFound) {
 			return &metricai.MetricAIIdList{}, nil
 		}
-		return nil, status.Errorf(codes.NotFound, "Failed to ListJobs for metricAI: %v", err)
+		return nil, status.Errorf(codes.NotFound, "Failed to ListJobs for metricAI: %s", err)
 	}
 	return &metricai.MetricAIIdList{Items: jobs}, nil
 }
@@ -141,14 +133,14 @@ func (p *MetricAnomaly) ListJobRuns(ctx context.Context, jobId *metricai.MetricA
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIRunKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to ListJobRuns for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to ListJobRuns for metricAI: %s", err)
 	}
 	jobruns, err := metricAIKeyValue.Keys()
 	if err != nil {
 		if errors.Is(err, nats.ErrNoKeysFound) {
 			return &metricai.MetricAIIdList{}, nil
 		}
-		return nil, status.Errorf(codes.NotFound, "Failed to ListJobRuns for metricAI: %v", err)
+		return nil, status.Errorf(codes.NotFound, "Failed to ListJobRuns for metricAI: %s", err)
 	}
 	var jobRunIdArray []string
 	// use jobId.Id + jobRunDelimiter to uniquely identify jobrun IDs for different jobs.
@@ -169,19 +161,30 @@ func (p *MetricAnomaly) RunJob(ctx context.Context, jobRequest *metricai.MetricA
 	url := p.MetricAnomalyServiceURL + "/run_job/" + jobRequest.Id
 	req, err := http.NewRequestWithContext(ctxca, http.MethodGet, url, nil)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to form httprequest to RunJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to form httprequest to RunJob for metricAI: %s", err)
 	}
 	resp, err := p.httpClient.Do(req)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to make httprequest to RunJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to make httprequest to RunJob for metricAI: %s", err)
 	}
 	defer resp.Body.Close()
 	var res map[string]interface{}
 
 	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to unmarshal response of SubmitJobRequest for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to unmarshal response of SubmitJobRequest for metricAI: %s", err)
 	}
 	return &metricai.MetricAIRunJobResponse{JobRunId: res["JobRunId"].(string), SubmittedTime: time.Now().String(), Status: res["Status"].(string)}, nil
+}
+
+func (p *MetricAnomaly) JobIdValidate(jobId string) error {
+	if jobId == "" { // id can't be empty
+		return validation.Error("jobId can't be empty")
+	}
+
+	if strings.Contains(jobId, jobRunDelimiter) { // disallow the delimiter. TODO: should only allow chars include alphanum and - and _
+		return validation.Error(fmt.Sprintf("jobId can't contain special char %s", jobRunDelimiter))
+	}
+	return nil
 }
 
 func (p *MetricAnomaly) CreateJob(ctx context.Context, jobRequest *metricai.MetricAICreateJobRequest) (*metricai.MetricAIAPIResponse, error) {
@@ -191,29 +194,16 @@ func (p *MetricAnomaly) CreateJob(ctx context.Context, jobRequest *metricai.Metr
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIJobKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to ListJobRuns for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to ListJobRuns for metricAI: %s", err)
 	}
 	jid := strings.ToLower(jobRequest.JobId)
-
-	if jid == "" { // id can't be empty
-		return nil, &RequestError{
-			StatusCode: 503,
-			Err:        errors.New("jobId can't be empty"),
-		}
-	}
-
-	if strings.Contains(jid, jobRunDelimiter) { // disallow the delimiter. TODO: should only allow chars include alphanum and - and _
-		return nil, &RequestError{
-			StatusCode: 503,
-			Err:        errors.New(fmt.Sprintf("jobId can't contain special char %s", jobRunDelimiter)),
-		}
+	err = p.JobIdValidate(jid)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Failed to CreateJob with jobID %s for metricAI, Error: %s", jid, err)
 	}
 
 	if _, err := metricAIKeyValue.Get(jid); err == nil { // check if this id exists
-		return nil, &RequestError{
-			StatusCode: 503,
-			Err:        errors.New("The jobId to add already exist"),
-		}
+		return nil, status.Errorf(codes.Internal, "Failed to CreateJob with jobID %s for metricAI, Error: The jobId to add already exist", jid)
 	}
 
 	job := make(map[string]interface{})
@@ -224,11 +214,11 @@ func (p *MetricAnomaly) CreateJob(ctx context.Context, jobRequest *metricai.Metr
 	job["JobDescription"] = jobRequest.JobDescription
 	jobJsonStr, err := json.Marshal(job)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to marshal JobStatus for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to marshal JobStatus for metricAI: %s", err)
 	}
 	_, err = metricAIKeyValue.Put(jid, []byte(jobJsonStr))
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to CreateJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to CreateJob for metricAI: %s", err)
 	}
 	return &metricai.MetricAIAPIResponse{SubmittedTime: time.Now().String(), Status: "Success"}, nil
 }
@@ -239,17 +229,14 @@ func (p *MetricAnomaly) DeleteJob(ctx context.Context, jobId *metricai.MetricAII
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIJobKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to DeleteJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to DeleteJob for metricAI: %s", err)
 	}
 	jid := jobId.Id
 	if _, err := metricAIKeyValue.Get(jid); err != nil {
 		if errors.Is(err, nats.ErrKeyNotFound) {
-			return nil, &RequestError{
-				StatusCode: 503,
-				Err:        errors.New("The jobId to delete doesn't exist"),
-			}
+			return nil, status.Errorf(codes.Internal, "Failed to DeleteJob for metricAI: The jobId to delete doesn't exist")
 		}
-		return nil, status.Errorf(codes.Internal, "Failed to DeleteJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to DeleteJob for metricAI: %s", err)
 	}
 	metricAIKeyValue.Delete(jid)
 	return &metricai.MetricAIAPIResponse{Status: "Success", Description: fmt.Sprintf("The JobId key %s is deleted", jid)}, nil
@@ -261,17 +248,14 @@ func (p *MetricAnomaly) DeleteJobRun(ctx context.Context, jobRunId *metricai.Met
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIRunKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to DeleteJobRun for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to DeleteJobRun for metricAI: %s", err)
 	}
 	jid := jobRunId.Id
 	if _, err := metricAIKeyValue.Get(jid); err != nil {
 		if errors.Is(err, nats.ErrKeyNotFound) {
-			return nil, &RequestError{
-				StatusCode: 503,
-				Err:        errors.New("The jobRunId to delete doesn't exist"),
-			}
+			return nil, status.Errorf(codes.Internal, "Failed to DeleteJobRun for metricAI: The jobRunId to delete doesn't exist")
 		}
-		return nil, status.Errorf(codes.Internal, "Failed to DeleteJobRun for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to DeleteJobRun for metricAI: %s", err)
 	}
 	metricAIKeyValue.Delete(jid)
 	return &metricai.MetricAIAPIResponse{Status: "Success", Description: fmt.Sprintf("The JobRunId key :%s is deleted", jid)}, nil
@@ -283,22 +267,19 @@ func (p *MetricAnomaly) GetJobRunResult(ctx context.Context, jobRunId *metricai.
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIRunKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to GetJobRunResult bucket for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to GetJobRunResult bucket for metricAI: %s", err)
 	}
 	jid := jobRunId.Id
 	jobRes, err := metricAIKeyValue.Get(jid)
 	if err != nil {
 		if errors.Is(err, nats.ErrKeyNotFound) {
-			return nil, &RequestError{
-				StatusCode: 503,
-				Err:        errors.New("The jobRunId doesn't exist"),
-			}
+			return nil, status.Errorf(codes.Internal, "Failed to GetJobRunResult with ID %s for metricAI: The jobRunId doesn't exist", jid)
 		}
-		return nil, status.Errorf(codes.Internal, "Failed to GetJobRunResult key %s for metricAI: %v", jid, err)
+		return nil, status.Errorf(codes.Internal, "Failed to GetJobRunResult key %s for metricAI: %s", jid, err)
 	}
 	var res = metricai.MetricAIJobRunResult{}
 	if err := json.Unmarshal(jobRes.Value(), &res); err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to unmarshal GetJobRunResult from Jetstream for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to unmarshal GetJobRunResult from Jetstream for metricAI: %s", err)
 	}
 	return &res, nil
 
@@ -310,22 +291,19 @@ func (p *MetricAnomaly) GetJob(ctx context.Context, jobId *metricai.MetricAIId) 
 	defer cancel()
 	metricAIKeyValue, err := p.metricAIJobKv.GetContext(ctxca)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to GetJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to GetJob for metricAI: %s", err)
 	}
 	jid := jobId.Id
 	jobRes, err := metricAIKeyValue.Get(jid)
 	if err != nil {
 		if errors.Is(err, nats.ErrKeyNotFound) {
-			return nil, &RequestError{
-				StatusCode: 503,
-				Err:        errors.New("The jobId doesn't exist"),
-			}
+			return nil, status.Errorf(codes.Internal, "Failed to GetJob for metricAI: The jobId doesn't exist")
 		}
-		return nil, status.Errorf(codes.Internal, "Failed to GetJob for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to GetJob for metricAI: %s", err)
 	}
 	var res = metricai.MetricAIJobStatus{}
 	if err := json.Unmarshal(jobRes.Value(), &res); err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to unmarshal JobStatus from Jetstream for metricAI: %v", err)
+		return nil, status.Errorf(codes.Internal, "Failed to unmarshal JobStatus from Jetstream for metricAI: %s", err)
 	}
 	return &res, nil
 

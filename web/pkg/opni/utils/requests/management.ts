7379ac284f0070cf52a367501e963da70481b0f6
codyrancher <cody@rancher.com>
2023-05-03 17:31:35 +0200

    Updating the path used for the mangement api

--- web/pkg/opni/utils/requests/management.ts
+++ web/pkg/opni/utils/requests/management.ts
@@ -10,11 +10,11 @@ import { LABEL_KEYS } from '../../models/shared';
 import { base64Encode } from '../crypto';
 
 export async function installCapabilityV2(capability: string, clusterId: string) {
-  return (await axios.post(`opni-api/management/clusters/${ clusterId }/capabilities/${ capability }/install`)).data;
+  return (await axios.post(`opni-api/Management/clusters/${ clusterId }/capabilities/${ capability }/install`)).data;
 }
 
 export async function uninstallCapabilityStatusV2(capability: string, clusterId: string) {
-  return await axios.get(`opni-api/management/clusters/${ clusterId }/capabilities/${ capability }/uninstall/status`);
+  return await axios.get(`opni-api/Management/clusters/${ clusterId }/capabilities/${ capability }/uninstall/status`);
 }
 
 export interface CapabilityInstallerResponse {
@@ -32,17 +32,17 @@ export interface DashboardSettings {
 }
 
 export async function getTokens(vue: any) {
-  const tokensResponse = (await axios.get<TokensResponse>(`opni-api/management/tokens`)).data.items;
+  const tokensResponse = (await axios.get<TokensResponse>(`opni-api/Management/tokens`)).data.items;
 
   return tokensResponse.map(tokenResponse => new Token(tokenResponse, vue));
 }
 
 export async function getDefaultImageRepository() {
-  return (await axios.get<DashboardSettings>(`opni-api/management/dashboard/settings`)).data.global?.defaultImageRepository;
+  return (await axios.get<DashboardSettings>(`opni-api/Management/dashboard/settings`)).data.global?.defaultImageRepository;
 }
 
 export async function getCapabilities(vue: any) {
-  const capabilitiesResponse = (await axios.get<any>(`opni-api/management/capabilities`)).data.items;
+  const capabilitiesResponse = (await axios.get<any>(`opni-api/Management/capabilities`)).data.items;
 
   return capabilitiesResponse;
 }
@@ -50,22 +50,22 @@ export async function getCapabilities(vue: any) {
 export function uninstallCapability(clusterId: string, capability: string, deleteStoredData: boolean, vue: any) {
   const initialDelay = deleteStoredData ? { initialDelay: '1m' } : {};
 
-  return axios.post<any>(`opni-api/management/clusters/${ clusterId }/capabilities/${ capability }/uninstall`, { options: { ...initialDelay, deleteStoredData } });
+  return axios.post<any>(`opni-api/Management/clusters/${ clusterId }/capabilities/${ capability }/uninstall`, { options: { ...initialDelay, deleteStoredData } });
 }
 
 export async function cancelCapabilityUninstall(clusterId: string, capabilityName: string) {
-  await axios.post(`opni-api/management/clusters/${ clusterId }/capabilities/${ capabilityName }/uninstall/cancel`, {
+  await axios.post(`opni-api/Management/clusters/${ clusterId }/capabilities/${ capabilityName }/uninstall/cancel`, {
     name:    capabilityName,
     cluster: { id: clusterId }
   });
 }
 
 export async function uninstallCapabilityStatus(clusterId: string, capability: string, vue: any) {
-  return (await axios.get<CapabilityStatusResponse>(`opni-api/management/clusters/${ clusterId }/capabilities/${ capability }/uninstall/status`)).data;
+  return (await axios.get<CapabilityStatusResponse>(`opni-api/Management/clusters/${ clusterId }/capabilities/${ capability }/uninstall/status`)).data;
 }
 
 export async function getCapabilityInstaller(capability: string, token: string, pin: string) {
-  return (await axios.post<CapabilityInstallerResponse>(`opni-api/management/capabilities/${ capability }/installer`, {
+  return (await axios.post<CapabilityInstallerResponse>(`opni-api/Management/capabilities/${ capability }/installer`, {
     token,
     pin,
   })).data.command;
@@ -74,7 +74,7 @@ export async function getCapabilityInstaller(capability: string, token: string, 
 export async function createToken(ttlInSeconds: string, name: string | null, capabilities: any[]) {
   const labels = name ? { labels: { [LABEL_KEYS.NAME]: name } } : { labels: {} };
 
-  const raw = (await axios.post<any>(`opni-api/management/tokens`, {
+  const raw = (await axios.post<any>(`opni-api/Management/tokens`, {
     ttl: ttlInSeconds,
     ...labels,
     capabilities,
@@ -84,7 +84,7 @@ export async function createToken(ttlInSeconds: string, name: string | null, cap
 }
 
 export function deleteToken(id: string): Promise<undefined> {
-  return axios.delete(`opni-api/management/tokens/${ id }`);
+  return axios.delete(`opni-api/Management/tokens/${ id }`);
 }
 
 export interface CertResponse {
@@ -101,7 +101,7 @@ export interface CertsResponse {
 }
 
 export async function getCerts(): Promise<CertResponse[]> {
-  return (await axios.get<CertsResponse>(`opni-api/management/certs`)).data.chain;
+  return (await axios.get<CertsResponse>(`opni-api/Management/certs`)).data.chain;
 }
 
 export async function getClusterFingerprint() {
@@ -115,15 +115,15 @@ export async function updateCluster(id: string, name: string, labels: { [key: st
   if (name === '') {
     delete labels[LABEL_KEYS.NAME];
   }
-  (await axios.put<any>(`opni-api/management/clusters/${ id }`, {
+  (await axios.put<any>(`opni-api/Management/clusters/${ id }`, {
     cluster: { id },
     labels
   }));
 }
 
 export async function getClusters(vue: any): Promise<Cluster[]> {
-  const clustersResponse = (await axios.get<ClustersResponse>(`opni-api/management/clusters`)).data.items;
-  const healthResponses = await Promise.allSettled(clustersResponse.map(clustersResponse => axios.get<HealthResponse>(`opni-api/management/clusters/${ clustersResponse.id }/health`)));
+  const clustersResponse = (await axios.get<ClustersResponse>(`opni-api/Management/clusters`)).data.items;
+  const healthResponses = await Promise.allSettled(clustersResponse.map(clustersResponse => axios.get<HealthResponse>(`opni-api/Management/clusters/${ clustersResponse.id }/health`)));
 
   const notConnected: HealthResponse = {
     status: {
@@ -142,49 +142,49 @@ export async function getClusters(vue: any): Promise<Cluster[]> {
 }
 
 export async function getCluster(id: string, vue: any) {
-  const clusterResponse = (await axios.get<ClusterResponse>(`opni-api/management/clusters/${ id }`)).data;
+  const clusterResponse = (await axios.get<ClusterResponse>(`opni-api/Management/clusters/${ id }`)).data;
 
   return new Cluster(clusterResponse, null as any, vue);
 }
 
 export function deleteCluster(id: string): Promise<undefined> {
-  return axios.delete(`opni-api/management/clusters/${ id }`);
+  return axios.delete(`opni-api/Management/clusters/${ id }`);
 }
 
 export async function getRoles(vue: any): Promise<Role[]> {
-  const rolesResponse = (await axios.get<RolesResponse>(`opni-api/management/roles`)).data.items;
+  const rolesResponse = (await axios.get<RolesResponse>(`opni-api/Management/roles`)).data.items;
 
   return rolesResponse.map(roleResponse => new Role(roleResponse, vue));
 }
 
 export function deleteRole(id: string): Promise<undefined> {
-  return axios.delete(`opni-api/management/roles/${ id }`);
+  return axios.delete(`opni-api/Management/roles/${ id }`);
 }
 
 export async function createRole(name: string, clusterIDs: string[], matchLabels: MatchLabel) {
-  (await axios.post<any>(`opni-api/management/roles`, {
+  (await axios.post<any>(`opni-api/Management/roles`, {
     id: name, clusterIDs, matchLabels
   }));
 }
 
 export async function getRoleBindings(vue: any): Promise<RoleBinding[]> {
-  const roleBindingsResponse = (await axios.get<RoleBindingsResponse>(`opni-api/management/rolebindings`)).data.items;
+  const roleBindingsResponse = (await axios.get<RoleBindingsResponse>(`opni-api/Management/rolebindings`)).data.items;
 
   return roleBindingsResponse.map(roleBindingResponse => new RoleBinding(roleBindingResponse, vue));
 }
 
 export function deleteRoleBinding(id: string): Promise<undefined> {
-  return axios.delete(`opni-api/management/rolebindings/${ id }`);
+  return axios.delete(`opni-api/Management/rolebindings/${ id }`);
 }
 
 export async function createRoleBinding(name: string, roleName: string, subjects: string[]) {
-  (await axios.post<any>(`opni-api/management/rolebindings`, {
+  (await axios.post<any>(`opni-api/Management/rolebindings`, {
     id: name, roleId: roleName, subjects
   }));
 }
 
 export async function getGatewayConfig(vue: any): Promise<ConfigDocument[]> {
-  const config = (await axios.get<GatewayConfig>(`opni-api/management/config`)).data;
+  const config = (await axios.get<GatewayConfig>(`opni-api/Management/config`)).data;
 
   return config.documents.map(configDocument => new ConfigDocument(configDocument, vue));
 }
@@ -196,5 +196,5 @@ export function updateGatewayConfig(jsonDocuments: string[]): Promise<undefined>
     documents.push({ json: base64Encode(jsonDocument) });
   }
 
-  return axios.put(`opni-api/management/config`, { documents });
+  return axios.put(`opni-api/Management/config`, { documents });
 }

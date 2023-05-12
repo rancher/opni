c414b7289b61d39e93b08e6a29763933261898a3
Joe Kralicky <joe.kralicky@suse.com>
2023-05-12 15:09:29 -0400

    update gogo compat init logic

--- plugins/metrics/pkg/apis/cortexadmin/compat.go
+++ plugins/metrics/pkg/apis/cortexadmin/compat.go
@@ -2,11 +2,9 @@ package cortexadmin
 
 import (
 	_ "github.com/cortexproject/cortex/pkg/cortexpb"
-	"github.com/jhump/protoreflect/desc"
 	"github.com/kralicky/ragu/compat"
 )
 
 func init() {
-	compat.LoadGogoFileDescriptor("cortex.proto")
-	desc.RegisterImportPath("cortex.proto", "github.com/cortexproject/cortex/pkg/cortexpb/cortex.proto")
+	compat.LoadGogoFileDescriptor("cortex.proto", compat.WithRename("github.com/cortexproject/cortex/pkg/cortexpb/cortex.proto"))
 }

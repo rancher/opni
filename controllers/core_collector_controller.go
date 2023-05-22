014ef60f0a5c32b23ceb607acf7bc01790aeefc6
Dan Bason <colonel.dan@gmail.com>
2023-05-22 13:48:56 +1200

    Update watches

--- controllers/core_collector_controller.go
+++ controllers/core_collector_controller.go
@@ -2,27 +2,21 @@ package controllers
 
 import (
 	"context"
-	"time"
 
 	promoperatorv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	corev1beta1 "github.com/rancher/opni/apis/core/v1beta1"
 	opniloggingv1beta1 "github.com/rancher/opni/apis/logging/v1beta1"
 	opnimonitoringv1beta1 "github.com/rancher/opni/apis/monitoring/v1beta1"
-	"github.com/rancher/opni/pkg/otel"
 	"github.com/rancher/opni/pkg/resources"
 	"github.com/rancher/opni/pkg/resources/collector"
 	"github.com/rancher/opni/pkg/util/k8sutil"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
@@ -77,61 +71,15 @@ func (r *CoreCollectorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	r.Client = mgr.GetClient()
 	r.scheme = mgr.GetScheme()
 
-	periodicRecChan := make(chan event.GenericEvent, 5)
-
-	go func() {
-		ticker := time.NewTicker(1 * time.Minute)
-		defer ticker.Stop()
-		for range ticker.C {
-			mockObj := &opnimonitoringv1beta1.CollectorConfig{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      otel.MetricsCrdName,
-					Namespace: "fake",
-				},
-			}
-			periodicRecChan <- event.GenericEvent{
-				Object: mockObj,
-			}
-		}
-	}()
-
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&corev1beta1.Collector{}).
 		Watches(&source.Kind{Type: &opnimonitoringv1beta1.CollectorConfig{}}, requestMapper).
 		Watches(&source.Kind{Type: &opniloggingv1beta1.CollectorConfig{}}, requestMapper).
 		// for metrics, the we want to watch changes to the spec of objects that drive discovery
-		Watches(
-			&source.Kind{Type: &promoperatorv1.ServiceMonitor{}},
-			requestMapper,
-			builder.WithPredicates(
-				predicate.Or(predicate.GenerationChangedPredicate{}),
-			),
-		).
-		Watches(
-			&source.Kind{Type: &promoperatorv1.PodMonitor{}},
-			requestMapper,
-			builder.WithPredicates(
-				predicate.Or(predicate.GenerationChangedPredicate{}),
-			),
-		).
-		Watches(
-			&source.Kind{Type: &corev1.Service{}},
-			requestMapper,
-			builder.WithPredicates(
-				predicate.Or(
-					predicate.GenerationChangedPredicate{},
-				),
-			),
-		).
-		// We need to watch almost all pod events here, but instead of queuing reconciles
-		// for each event, we "hack" a batch mechanism for potential pod events
-		// using a periodic reconciliation channel.
-		Watches(
-			&source.Channel{
-				Source: periodicRecChan,
-			},
-			requestMapper,
-		).
+		Watches(&source.Kind{Type: &promoperatorv1.ServiceMonitor{}}, &handler.EnqueueRequestForObject{}).
+		Watches(&source.Kind{Type: &promoperatorv1.PodMonitor{}}, &handler.EnqueueRequestForObject{}).
+		Watches(&source.Kind{Type: &corev1.Service{}}, &handler.EnqueueRequestForObject{}).
+		Watches(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForObject{}).
 		Owns(&corev1.ConfigMap{}).
 		Owns(&corev1.Service{}).
 		Owns(&appsv1.DaemonSet{}).

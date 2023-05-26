fd175d3ddc7e7f05bccad469c7107cd147207e06
codyrancher <cody@rancher.com>
2023-05-26 20:53:01 +0300

    Removing what appears to be test code
    We were setting the metric capability when getting loggingCapabilities. I believe that this was test code because I can't see any reason why this was there.
    
    https://github.com/rancher/opni/issues/1450

--- web/pkg/opni/utils/requests/capability.ts
+++ web/pkg/opni/utils/requests/capability.ts
@@ -1,12 +1,10 @@
 import { MetricCapability } from '../../models/MetricCapability';
 import { Capability } from '../../models/Capability';
 import { getClusters } from './management';
-import { getNodeConfiguration, setNodeConfiguration } from './node-configuration';
+import { getNodeConfiguration } from './node-configuration';
 
 export async function getLoggingCapabilities(vue: any): Promise<Capability[]> {
   const clusters = getClusters(vue);
-
-  (await clusters).forEach(c => setNodeConfiguration(c.id, 'Prometheus'));
 
   return (await clusters).map(c => new Capability('logs', c, vue));
 }

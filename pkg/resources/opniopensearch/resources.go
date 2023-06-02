c2ad957dab269362da57af2c63000c1e85819040
Amartya Chakraborty <chakrab.amartya@gmail.com>
2023-06-02 14:37:23 -0700

    Update files for v0.1.0 rc6 release

--- pkg/resources/opniopensearch/resources.go
+++ pkg/resources/opniopensearch/resources.go
@@ -45,7 +45,7 @@ func (r *Reconciler) buildOpensearchCluster(
 	// Set default image version
 	version := r.instance.Spec.Version
 	if version == "unversioned" {
-		version = "0.10.0-rc5"
+		version = "0.10.0-rc6"
 	}
 
 	image := fmt.Sprintf(
--- plugins/logging/pkg/gateway/admin_v2.go
+++ plugins/logging/pkg/gateway/admin_v2.go
@@ -24,7 +24,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-const defaultOpniVersion = "0.10.0-rc5"
+const defaultOpniVersion = "0.10.0-rc6"
 
 type ClusterStatus int
 

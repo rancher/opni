bd3cc81d83992cb329667e331a220b43e72eb576
Dan Bason <colonel.dan@gmail.com>
2023-07-27 13:04:17 +1200

    Add contstraints to generate crds

--- magefiles/targets/generate_crd.go
+++ magefiles/targets/generate_crd.go
@@ -5,17 +5,21 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io/fs"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"github.com/magefile/mage/target"
 	"github.com/samber/lo"
 )
 
 type CRD mg.Namespace
+
+var runReplace bool
 
 func (CRD) All(ctx context.Context) {
 	ctx, tr := Tracer.Start(ctx, "target.crd")
@@ -26,47 +30,55 @@ func (CRD) All(ctx context.Context) {
 func (CRD) CRDGen(ctx context.Context) error {
 	_, tr := Tracer.Start(ctx, "target.crd.crdgen")
 	defer tr.End()
-	var commands []*exec.Cmd
-	commands = append(commands, exec.Command(mg.GoCmd(), "run", "sigs.k8s.io/kustomize/kustomize/v5",
-		"build", "./config/chart-crds", "-o", "./packages/opni/opni/charts/crds/crds.yaml",
-	))
-	for _, cmd := range commands {
-		buf := new(bytes.Buffer)
-		cmd.Stderr = buf
-		cmd.Stdout = buf
-		err := cmd.Run()
-		if err != nil {
-			if ex, ok := err.(*exec.ExitError); ok {
-				if ex.ExitCode() != 1 {
-					return errors.New(buf.String())
-				}
-				bufStr := buf.String()
-				lines := strings.Split(bufStr, "\n")
-				for _, line := range lines {
-					if strings.TrimSpace(line) == "" {
-						continue
+
+	ok, err := checkCRDContstraints()
+	if err != nil {
+		return err
+	}
+	if ok {
+		runReplace = true
+		var commands []*exec.Cmd
+		commands = append(commands, exec.Command(mg.GoCmd(), "run", "sigs.k8s.io/kustomize/kustomize/v5",
+			"build", "./config/chart-crds", "-o", "./packages/opni/opni/charts/crds/crds.yaml",
+		))
+		for _, cmd := range commands {
+			buf := new(bytes.Buffer)
+			cmd.Stderr = buf
+			cmd.Stdout = buf
+			err := cmd.Run()
+			if err != nil {
+				if ex, ok := err.(*exec.ExitError); ok {
+					if ex.ExitCode() != 1 {
+						return errors.New(buf.String())
 					}
-					fmt.Fprintln(os.Stderr, line)
-					return err
+					bufStr := buf.String()
+					lines := strings.Split(bufStr, "\n")
+					for _, line := range lines {
+						if strings.TrimSpace(line) == "" {
+							continue
+						}
+						fmt.Fprintln(os.Stderr, line)
+						return err
+					}
 				}
 			}
 		}
-	}
 
-	expr := `del(.. | select(has("description")).description | select(has("type") | not )) | .. style="flow"`
+		expr := `del(.. | select(has("description")).description | select(has("type") | not )) | .. style="flow"`
 
-	e1 := lo.Async(func() error {
-		return sh.Run(mg.GoCmd(), "run", "github.com/mikefarah/yq/v4", "-i", expr, "./packages/opni/opni/charts/crds/crds.yaml")
-	})
+		e1 := lo.Async(func() error {
+			return sh.Run(mg.GoCmd(), "run", "github.com/mikefarah/yq/v4", "-i", expr, "./packages/opni/opni/charts/crds/crds.yaml")
+		})
 
-	if err := <-e1; err != nil {
-		return err
-	}
-
-	// prepend "---" to each file, otherwise kubernetes will think it's json
-	for _, f := range []string{"./packages/opni/opni/charts/crds/crds.yaml"} {
-		if err := prependDocumentSeparator(f); err != nil {
+		if err := <-e1; err != nil {
 			return err
+		}
+
+		// prepend "---" to each file, otherwise kubernetes will think it's json
+		for _, f := range []string{"./packages/opni/opni/charts/crds/crds.yaml"} {
+			if err := prependDocumentSeparator(f); err != nil {
+				return err
+			}
 		}
 	}
 
@@ -105,22 +117,62 @@ func prependDocumentSeparator(path string) error {
 func (CRD) ReplaceCRDText(ctx context.Context) error {
 	_, tr := Tracer.Start(ctx, "target.crd.replacecrdtext")
 	defer tr.End()
-	files := []string{
-		"./packages/opni/opni/charts/crds/crds.yaml",
-	}
 
-	for _, file := range files {
-		input, err := ioutil.ReadFile(file)
+	if !runReplace {
+		ok, err := checkCRDContstraints()
 		if err != nil {
 			return err
 		}
+		runReplace = ok
+	}
+	if runReplace {
+		files := []string{
+			"./packages/opni/opni/charts/crds/crds.yaml",
+		}
 
-		firstReplace := bytes.Replace(input, []byte("replace-me/opni-serving-cert"), []byte(`"replace-me/opni-serving-cert"`), -1)
-		output := bytes.Replace(firstReplace, []byte("replace-me"), []byte("{{ .Release.Namespace }}"), -1)
+		for _, file := range files {
+			input, err := os.ReadFile(file)
+			if err != nil {
+				return err
+			}
 
-		if err := ioutil.WriteFile(file, output, 0644); err != nil {
-			return err
+			firstReplace := bytes.Replace(input, []byte("replace-me/opni-serving-cert"), []byte(`"replace-me/opni-serving-cert"`), -1)
+			output := bytes.Replace(firstReplace, []byte("replace-me"), []byte("{{ .Release.Namespace }}"), -1)
+
+			if err := os.WriteFile(file, output, 0644); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
+}
+
+func checkCRDContstraints() (bool, error) {
+	var crdSources []string
+	err := filepath.WalkDir("config/crd", func(path string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(entry.Name(), ".yaml") {
+			crdSources = append(crdSources, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+	err = filepath.WalkDir("config/chart-crds", func(path string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if strings.HasSuffix(entry.Name(), ".yaml") {
+			crdSources = append(crdSources, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return target.Dir("packages/opni/opni/charts/crds", crdSources...)
 }

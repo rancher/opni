cda329dcdd7f83c3c2f07419e76eedb826493e38
Dan Bason <colonel.dan@gmail.com>
2023-08-09 10:16:26 +1200

    Update RC version

--- test/plugins/aiops/admin_test.go
+++ test/plugins/aiops/admin_test.go
@@ -530,7 +530,7 @@ var _ = Describe("AI Admin", Ordered, Label("integration"), func() {
 		})
 		When("new version is available", func() {
 			BeforeEach(func() {
-				version = "v0.11.0-rc3"
+				version = "v0.11.0-rc4"
 			})
 			When("updating the opni cluster", func() {
 				BeforeEach(func() {
@@ -569,7 +569,7 @@ var _ = Describe("AI Admin", Ordered, Label("integration"), func() {
 					Name:      OpniServicesName,
 					Namespace: namespace,
 				}, cluster)).Should(Succeed())
-				Expect(cluster.Spec.Version).To(Equal("v0.11.0-rc3"))
+				Expect(cluster.Spec.Version).To(Equal("v0.11.0-rc4"))
 			})
 		})
 		When("new version is older", func() {

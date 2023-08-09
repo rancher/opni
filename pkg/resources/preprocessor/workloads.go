6351df0cd8604b66b6ae02bf20cc080c0cd38851
João Henri <joao.rocha@suse.com>
2023-08-09 11:48:36 -0300

    Add no_op processor for Trace pipeline
    Signed-off-by: João Henri <joao.rocha@suse.com>

--- pkg/resources/preprocessor/workloads.go
+++ pkg/resources/preprocessor/workloads.go
@@ -81,6 +81,9 @@ processors:
       statements:
       - set(attributes["log_type"], "controlplane") where attributes["k8s.pod.labels.tier"] == "control-plane"
       - set(attributes["kubernetes_component"], attributes["k8s.pod.labels.component"]) where attributes["k8s.pod.labels.tier"] == "control-plane"
+  no_op:
+    name: no_op
+
 exporters:
   opensearch:
     endpoints: [ "{{ .Endpoint }}" ]
@@ -98,6 +101,10 @@ service:
     logs:
       receivers: ["otlp"]
       processors: ["resource", "attributes", "transform"]
+      exporters: ["opensearch"]
+	traces:
+ 	  receivers: ["otlp"]
+      processors: ["no_op"]
       exporters: ["opensearch"]
 `))
 )

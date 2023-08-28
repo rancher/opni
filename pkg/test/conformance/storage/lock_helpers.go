f4c373429ee2b4f048eb50d05eedfbf6a0c0f8ad
Alexandre Lamarre <alexandre.lamarre@suse.com>
2023-08-28 14:03:59 -0400

    Distributed locking conformance tests

---
+++ pkg/test/conformance/storage/lock_helpers.go
@@ -0,0 +1,226 @@
+package conformance_storage
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/rancher/opni/pkg/storage"
+	"github.com/rancher/opni/pkg/storage/lock"
+	"github.com/samber/lo"
+)
+
+// Timeouts are only valid if we specified lock.Keeplive(false)
+func expectTimeout(lm storage.LockManager) error {
+	lock1 := lm.Locker("bar", lock.WithKeepalive(false), lock.WithAcquireTimeout(time.Second), lock.WithExpireDuration(5*time.Second))
+	lock2 := lm.Locker("bar")
+
+	err := lock1.Lock()
+	if err != nil {
+		return err
+	}
+
+	err = lock2.Lock()
+	if !errors.Is(err, lock.ErrAcquireLockTimeout) {
+		return fmt.Errorf("expected lock to timeout, but got %v", err)
+	}
+
+	err = lock2.Unlock()
+	if !errors.Is(err, lock.ErrLockNotAcquired) {
+		return fmt.Errorf("expected unlock to fail, but got %v", err)
+	}
+
+	return lock1.Unlock()
+}
+
+func expectCancellable(lm storage.LockManager) error {
+	ctxca, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	exLock := lm.Locker("bar", lock.WithExpireDuration(time.Hour))
+	cancelLock := lm.Locker("bar", lock.WithAcquireContext(ctxca), lock.WithRetryDelay(time.Microsecond*500))
+	err := exLock.Lock()
+	if err != nil {
+		return err
+	}
+	go func() {
+		time.Sleep(500 * time.Microsecond)
+		cancel()
+	}()
+	err = cancelLock.Lock()
+	if !errors.Is(err, lock.ErrAcquireLockCancelled) {
+		return fmt.Errorf("expected lock to be cancelled, but got %v", err)
+	}
+
+	err = cancelLock.Unlock()
+	if !errors.Is(err, lock.ErrLockNotAcquired) {
+		return fmt.Errorf("expected unlock to fail, but got %v", err)
+	}
+	return exLock.Unlock()
+
+}
+
+type lockWithTransaction lo.Tuple3[storage.Lock, *transaction, chan struct{}]
+
+// Sequential execution is guaranteed by :
+// - first starting a lock
+// - having multiple candidate locks try and concurrently acquire the lock
+// - when the first lock is released, only one of the candidate locks should be able to acquire the lock
+func expectAtomic(lock1 storage.Lock, lockConstructor func(opts ...lock.LockOption) storage.Lock) error {
+	err := lock1.Lock()
+	Expect(err).NotTo(HaveOccurred())
+
+	otherLocks := []storage.Lock{
+		lockConstructor(lock.WithExpireDuration(time.Second), lock.WithAcquireTimeout(50*time.Millisecond)),
+		lockConstructor(lock.WithExpireDuration(time.Second), lock.WithAcquireTimeout(50*time.Millisecond)),
+		lockConstructor(lock.WithExpireDuration(time.Second), lock.WithAcquireTimeout(50*time.Millisecond)),
+	}
+
+	var numLocksAcquired uint32
+	var numLocksTimeouted uint32
+	var tasksSpawned uint32
+	expectedTasks := len(otherLocks)
+
+	var wg sync.WaitGroup
+	var toUnlock storage.Lock
+
+	wg.Add(2)
+
+	go func() {
+		var nestedWg sync.WaitGroup
+		defer wg.Done()
+		defer GinkgoRecover()
+		nestedWg.Add(len(otherLocks))
+		for _, olock := range otherLocks {
+			olock := olock
+			go func() {
+				defer nestedWg.Done()
+				atomic.AddUint32(&tasksSpawned, 1)
+				err := olock.Lock()
+				if err == nil {
+					atomic.AddUint32(&numLocksAcquired, 1)
+					toUnlock = olock
+				}
+				if errors.Is(err, lock.ErrAcquireLockTimeout) {
+					atomic.AddUint32(&numLocksTimeouted, 1)
+				}
+			}()
+		}
+		nestedWg.Wait()
+
+	}()
+
+	go func() {
+		for {
+			tasks := atomic.LoadUint32(&tasksSpawned)
+			if tasks == uint32(expectedTasks) {
+				break
+			}
+		}
+		defer GinkgoRecover()
+		lock1.Unlock()
+		Expect(err).NotTo(HaveOccurred())
+		defer wg.Done()
+	}()
+
+	wg.Wait()
+	if toUnlock == nil {
+		return fmt.Errorf("expected one of the locks to be acquired, but none were (%d timeouted)", numLocksTimeouted)
+	}
+	if err := toUnlock.Unlock(); err != nil {
+		return err
+	}
+	Expect(err).NotTo(HaveOccurred())
+	if numLocksAcquired != 1 {
+		return fmt.Errorf("expected only one lock to be acquired, but got %d", numLocksAcquired)
+	}
+	if numLocksTimeouted != uint32(len(otherLocks)-1) {
+		return fmt.Errorf("expected %d locks to timeout, but got %d", len(otherLocks)-1, numLocksTimeouted)
+	}
+	return nil
+}
+
+func expectAtomicKeepAlive(lock1 storage.Lock, lockConstructor func(opts ...lock.LockOption) storage.Lock) error {
+	err := lock1.Lock()
+	Expect(err).NotTo(HaveOccurred())
+
+	otherLocks := []storage.Lock{
+		lockConstructor(lock.WithExpireDuration(time.Second), lock.WithAcquireTimeout(50*time.Millisecond)),
+		lockConstructor(lock.WithExpireDuration(time.Second), lock.WithAcquireTimeout(50*time.Millisecond)),
+		lockConstructor(lock.WithExpireDuration(time.Second), lock.WithAcquireTimeout(50*time.Millisecond)),
+	}
+
+	var numLocksAcquired uint32
+	var numLocksTimeouted uint32
+	var tasksSpawned uint32
+	expectedTasks := len(otherLocks)
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		var nestedWg sync.WaitGroup
+		defer wg.Done()
+		defer GinkgoRecover()
+		nestedWg.Add(len(otherLocks))
+		for _, olock := range otherLocks {
+			olock := olock
+			go func() {
+				defer nestedWg.Done()
+				atomic.AddUint32(&tasksSpawned, 1)
+				err := olock.Lock()
+				if err == nil {
+					atomic.AddUint32(&numLocksAcquired, 1)
+				}
+				if errors.Is(err, lock.ErrAcquireLockTimeout) {
+					atomic.AddUint32(&numLocksTimeouted, 1)
+				}
+			}()
+		}
+		nestedWg.Wait()
+
+	}()
+
+	go func() {
+		for {
+			tasks := atomic.LoadUint32(&tasksSpawned)
+			if tasks == uint32(expectedTasks) {
+				break
+			}
+		}
+		defer wg.Done()
+	}()
+
+	wg.Wait()
+	Expect(err).NotTo(HaveOccurred())
+	if numLocksAcquired != 0 {
+		return fmt.Errorf("expected no locks to be acquired, but got %d", numLocksAcquired)
+	}
+	if numLocksTimeouted != uint32(len(otherLocks)) {
+		return fmt.Errorf("expected all %d locks to timeout, but got %d", len(otherLocks), numLocksTimeouted)
+	}
+	return nil
+}
+
+type transaction struct {
+	Uuid string
+}
+
+func jitter() time.Duration {
+	rand.NewSource(time.Now().UnixNano())
+	randomMicroseconds := rand.Intn(101)
+	jitterInterval := time.Duration(randomMicroseconds) * time.Microsecond
+	return jitterInterval
+}
+
+func sendWithJitter(send chan struct{}) {
+	<-time.After(jitter())
+	send <- struct{}{}
+}
---
+++ pkg/test/conformance/storage/lock_manager.go
@@ -0,0 +1,134 @@
+package conformance_storage
+
+import (
+	"sync"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/rancher/opni/pkg/storage"
+	"github.com/rancher/opni/pkg/storage/lock"
+	"github.com/rancher/opni/pkg/util/future"
+	"github.com/samber/lo"
+)
+
+func LockManagerTestSuite[T storage.LockManager](
+	lmF future.Future[T],
+) func() {
+	return func() {
+		var lm T
+		BeforeAll(func() {
+			lm = lmF.Get()
+		})
+
+		When("using distributed locks ", func() {
+			It("should only request lock actions once", func() {
+				lock1 := lm.Locker("foo")
+				err := lock1.Lock()
+				Expect(err).NotTo(HaveOccurred())
+				err = lock1.Lock()
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(lock.ErrLockActionRequested))
+
+				err = lock1.Unlock()
+				Expect(err).NotTo(HaveOccurred())
+				err = lock1.Unlock()
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError(lock.ErrLockActionRequested))
+			})
+
+			It("should lock and unlock locks of the same type", func() {
+				lock1 := lm.Locker("todo")
+				Expect(lock1.Lock()).To(Succeed())
+				Expect(lock1.Unlock()).To(Succeed())
+
+				lock2 := lm.Locker("todo")
+				Expect(lock2.Lock()).To(Succeed())
+				Expect(lock2.Unlock()).To(Succeed())
+			})
+			It("should resolve concurrent lock requests", func() {
+				locks := []lockWithTransaction{}
+				for i := 0; i < 2; i++ {
+					lock := lm.Locker("todo")
+					locks = append(locks, lockWithTransaction{
+						A: lock,
+						C: make(chan struct{}),
+					})
+				}
+				errs := make(chan error, 2*len(locks))
+
+				lockOrder := lo.Shuffle(locks)
+				var wg sync.WaitGroup
+				for _, lock := range lockOrder {
+					lock := lock
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						err := lock.A.Lock()
+						sendWithJitter(lock.C)
+						errs <- err
+					}()
+				}
+				for _, lock := range lockOrder {
+					lock := lock
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						<-lock.C
+						errs <- lock.A.Unlock()
+					}()
+				}
+				wg.Wait()
+				Eventually(errs).Should(Receive(BeNil()))
+			})
+
+			Specify("locks should be able to acquire the lock if the existing lock has expired", func() {
+				exLock := lm.Locker("bar", lock.WithRetryDelay(1*time.Millisecond), lock.WithExpireDuration(time.Millisecond*50))
+				otherLock := lm.Locker("bar", lock.WithAcquireTimeout(time.Second))
+				err := exLock.Lock()
+				Expect(err).NotTo(HaveOccurred())
+
+				time.Sleep(2 * time.Second)
+
+				err = otherLock.Lock()
+				Expect(err).NotTo(HaveOccurred())
+
+				By("releasing the expired exclusive lock should not affect releasing newer locks")
+				err = exLock.Unlock()
+				Expect(err).NotTo(HaveOccurred())
+
+				err = otherLock.Unlock()
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			Specify("any lock should timeout after the specified timeout", func() {
+				Expect(expectTimeout(lm)).To(Succeed())
+
+			})
+
+			Specify("lock should respect their context errors", func() {
+				Expect(expectCancellable(lm)).To(Succeed())
+			})
+		})
+
+		When("using exclusive locks", func() {
+			It("should allow multiple writers to safely write using locks", func() {
+				lock1 := lm.Locker("foo2", lock.WithKeepalive(false), lock.WithExpireDuration(10*time.Millisecond))
+				err := expectAtomic(lock1, func(opts ...lock.LockOption) storage.Lock {
+					return lm.Locker("foo2", opts...)
+				})
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should block other locks from acquiring the lock if the process holding the lock is alive", func() {
+				lock1 := lm.Locker("foo3", lock.WithKeepalive(true), lock.WithExpireDuration(1*time.Millisecond))
+				err := expectAtomicKeepAlive(lock1, func(opts ...lock.LockOption) storage.Lock {
+					return lm.Locker("foo3", opts...)
+				})
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+	}
+}

386285f72278e1844de3fe51c3321e82e64adaf6
Joe Kralicky <joe.kralicky@suse.com>
2023-09-11 18:31:33 -0400

    Add config server implementation in the example plugin

---
+++ plugins/example/pkg/example/driver.go
@@ -0,0 +1,134 @@
+package example
+
+import (
+	context "context"
+	"os"
+
+	cli "github.com/rancher/opni/internal/codegen/cli"
+	driverutil "github.com/rancher/opni/pkg/plugins/driverutil"
+	"github.com/rancher/opni/pkg/plugins/driverutil/rollback"
+	"github.com/rancher/opni/pkg/storage"
+	"github.com/rancher/opni/pkg/tui"
+	"github.com/rancher/opni/pkg/util"
+	flagutil "github.com/rancher/opni/pkg/util/flagutil"
+	"golang.org/x/term"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type ExampleDriver interface {
+	driverutil.ConfigServer[*ConfigSpec, *ResetRequest, *HistoryResponse]
+	driverutil.DryRunServer[*ConfigSpec, *DryRunRequest, *DryRunResponse]
+}
+
+type DriverImpl struct {
+	*driverutil.BaseConfigServer[*ResetRequest, *HistoryResponse, *ConfigSpec]
+}
+
+var drivers = driverutil.NewDriverCache[ExampleDriver]()
+
+type ExampleDriverImplOptions struct {
+	DefaultConfigStore storage.ValueStoreT[*ConfigSpec] `option:"defaultConfigStore"`
+	ActiveConfigStore  storage.ValueStoreT[*ConfigSpec] `option:"activeConfigStore"`
+}
+
+func NewExampleDriverImpl(options ExampleDriverImplOptions) *DriverImpl {
+	return &DriverImpl{
+		BaseConfigServer: driverutil.NewBaseConfigServer[*ResetRequest, *HistoryResponse](
+			options.DefaultConfigStore,
+			options.ActiveConfigStore,
+			flagutil.LoadDefaults,
+		),
+	}
+}
+
+// DryRun implements ExampleDriver.
+func (d *DriverImpl) DryRun(ctx context.Context, req *DryRunRequest) (*DryRunResponse, error) {
+	// The tracker implements all the logic for DryRun except for validation.
+	results, err := d.Tracker().DryRun(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return &DryRunResponse{
+		Current:          results.Current,
+		Modified:         results.Modified,
+		ValidationErrors: nil, // This is left as an implementation detail.
+	}, nil
+}
+
+// Enables the history terminal UI
+func (h *HistoryResponse) RenderText(out cli.Writer) {
+	if !term.IsTerminal(int(os.Stdout.Fd())) {
+		out.Println(driverutil.MarshalConfigJson(h))
+		return
+	}
+	ui := tui.NewHistoryUI(h.GetEntries())
+	ui.Run()
+}
+
+func init() {
+	// Adds the rollback command
+	addExtraConfigCmd(rollback.BuildCmd("rollback", ConfigClientFromContext))
+
+	drivers.Register("example", func(ctx context.Context, opts ...driverutil.Option) (ExampleDriver, error) {
+		options := ExampleDriverImplOptions{}
+		if err := driverutil.ApplyOptions(&options, opts...); err != nil {
+			return nil, err
+		}
+		return NewExampleDriverImpl(options), nil
+	})
+}
+
+type ConfigServerBackend struct {
+	util.Initializer
+	UnsafeConfigServer
+
+	driver ExampleDriver
+}
+
+func (b *ConfigServerBackend) Initialize(driver ExampleDriver) {
+	b.InitOnce(func() {
+		b.driver = driver
+	})
+}
+
+func (b *ConfigServerBackend) ConfigurationHistory(ctx context.Context, in *driverutil.ConfigurationHistoryRequest) (*HistoryResponse, error) {
+	b.WaitForInit()
+	return b.driver.ConfigurationHistory(ctx, in)
+}
+
+func (b *ConfigServerBackend) DryRun(ctx context.Context, in *DryRunRequest) (*DryRunResponse, error) {
+	b.WaitForInit()
+	return b.driver.DryRun(ctx, in)
+}
+
+func (b *ConfigServerBackend) GetConfiguration(ctx context.Context, in *driverutil.GetRequest) (*ConfigSpec, error) {
+	b.WaitForInit()
+	return b.driver.GetConfiguration(ctx, in)
+}
+
+func (b *ConfigServerBackend) GetDefaultConfiguration(ctx context.Context, in *driverutil.GetRequest) (*ConfigSpec, error) {
+	b.WaitForInit()
+	return b.driver.GetDefaultConfiguration(ctx, in)
+}
+
+func (b *ConfigServerBackend) ResetConfiguration(ctx context.Context, in *ResetRequest) (*emptypb.Empty, error) {
+	b.WaitForInit()
+	return b.driver.ResetConfiguration(ctx, in)
+}
+
+func (b *ConfigServerBackend) ResetDefaultConfiguration(ctx context.Context, in *emptypb.Empty) (*emptypb.Empty, error) {
+	b.WaitForInit()
+	return b.driver.ResetDefaultConfiguration(ctx, in)
+}
+
+func (b *ConfigServerBackend) SetConfiguration(ctx context.Context, in *ConfigSpec) (*emptypb.Empty, error) {
+	b.WaitForInit()
+	return b.driver.SetConfiguration(ctx, in)
+}
+
+func (b *ConfigServerBackend) SetDefaultConfiguration(ctx context.Context, in *ConfigSpec) (*emptypb.Empty, error) {
+	b.WaitForInit()
+	return b.driver.SetDefaultConfiguration(ctx, in)
+}
+
+var _ ConfigServer = (*ConfigServerBackend)(nil)
--- plugins/example/pkg/example/example_grpc.pb.go
+++ plugins/example/pkg/example/example_grpc.pb.go
@@ -8,6 +8,7 @@ package example
 
 import (
 	context "context"
+	driverutil "github.com/rancher/opni/pkg/plugins/driverutil"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
@@ -230,6 +231,355 @@ var ExampleUnaryExtension_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Hello",
 			Handler:    _ExampleUnaryExtension_Hello_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "github.com/rancher/opni/plugins/example/pkg/example/example.proto",
+}
+
+const (
+	Config_GetDefaultConfiguration_FullMethodName   = "/example.Config/GetDefaultConfiguration"
+	Config_SetDefaultConfiguration_FullMethodName   = "/example.Config/SetDefaultConfiguration"
+	Config_GetConfiguration_FullMethodName          = "/example.Config/GetConfiguration"
+	Config_SetConfiguration_FullMethodName          = "/example.Config/SetConfiguration"
+	Config_ResetDefaultConfiguration_FullMethodName = "/example.Config/ResetDefaultConfiguration"
+	Config_ResetConfiguration_FullMethodName        = "/example.Config/ResetConfiguration"
+	Config_DryRun_FullMethodName                    = "/example.Config/DryRun"
+	Config_ConfigurationHistory_FullMethodName      = "/example.Config/ConfigurationHistory"
+)
+
+// ConfigClient is the client API for Config service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type ConfigClient interface {
+	GetDefaultConfiguration(ctx context.Context, in *driverutil.GetRequest, opts ...grpc.CallOption) (*ConfigSpec, error)
+	SetDefaultConfiguration(ctx context.Context, in *ConfigSpec, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	GetConfiguration(ctx context.Context, in *driverutil.GetRequest, opts ...grpc.CallOption) (*ConfigSpec, error)
+	SetConfiguration(ctx context.Context, in *ConfigSpec, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	ResetDefaultConfiguration(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	ResetConfiguration(ctx context.Context, in *ResetRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	DryRun(ctx context.Context, in *DryRunRequest, opts ...grpc.CallOption) (*DryRunResponse, error)
+	ConfigurationHistory(ctx context.Context, in *driverutil.ConfigurationHistoryRequest, opts ...grpc.CallOption) (*HistoryResponse, error)
+}
+
+type configClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewConfigClient(cc grpc.ClientConnInterface) ConfigClient {
+	return &configClient{cc}
+}
+
+func (c *configClient) GetDefaultConfiguration(ctx context.Context, in *driverutil.GetRequest, opts ...grpc.CallOption) (*ConfigSpec, error) {
+	out := new(ConfigSpec)
+	err := c.cc.Invoke(ctx, Config_GetDefaultConfiguration_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) SetDefaultConfiguration(ctx context.Context, in *ConfigSpec, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Config_SetDefaultConfiguration_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) GetConfiguration(ctx context.Context, in *driverutil.GetRequest, opts ...grpc.CallOption) (*ConfigSpec, error) {
+	out := new(ConfigSpec)
+	err := c.cc.Invoke(ctx, Config_GetConfiguration_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) SetConfiguration(ctx context.Context, in *ConfigSpec, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Config_SetConfiguration_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) ResetDefaultConfiguration(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Config_ResetDefaultConfiguration_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) ResetConfiguration(ctx context.Context, in *ResetRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Config_ResetConfiguration_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) DryRun(ctx context.Context, in *DryRunRequest, opts ...grpc.CallOption) (*DryRunResponse, error) {
+	out := new(DryRunResponse)
+	err := c.cc.Invoke(ctx, Config_DryRun_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *configClient) ConfigurationHistory(ctx context.Context, in *driverutil.ConfigurationHistoryRequest, opts ...grpc.CallOption) (*HistoryResponse, error) {
+	out := new(HistoryResponse)
+	err := c.cc.Invoke(ctx, Config_ConfigurationHistory_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// ConfigServer is the server API for Config service.
+// All implementations must embed UnimplementedConfigServer
+// for forward compatibility
+type ConfigServer interface {
+	GetDefaultConfiguration(context.Context, *driverutil.GetRequest) (*ConfigSpec, error)
+	SetDefaultConfiguration(context.Context, *ConfigSpec) (*emptypb.Empty, error)
+	GetConfiguration(context.Context, *driverutil.GetRequest) (*ConfigSpec, error)
+	SetConfiguration(context.Context, *ConfigSpec) (*emptypb.Empty, error)
+	ResetDefaultConfiguration(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
+	ResetConfiguration(context.Context, *ResetRequest) (*emptypb.Empty, error)
+	DryRun(context.Context, *DryRunRequest) (*DryRunResponse, error)
+	ConfigurationHistory(context.Context, *driverutil.ConfigurationHistoryRequest) (*HistoryResponse, error)
+	mustEmbedUnimplementedConfigServer()
+}
+
+// UnimplementedConfigServer must be embedded to have forward compatible implementations.
+type UnimplementedConfigServer struct {
+}
+
+func (UnimplementedConfigServer) GetDefaultConfiguration(context.Context, *driverutil.GetRequest) (*ConfigSpec, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetDefaultConfiguration not implemented")
+}
+func (UnimplementedConfigServer) SetDefaultConfiguration(context.Context, *ConfigSpec) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetDefaultConfiguration not implemented")
+}
+func (UnimplementedConfigServer) GetConfiguration(context.Context, *driverutil.GetRequest) (*ConfigSpec, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetConfiguration not implemented")
+}
+func (UnimplementedConfigServer) SetConfiguration(context.Context, *ConfigSpec) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetConfiguration not implemented")
+}
+func (UnimplementedConfigServer) ResetDefaultConfiguration(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ResetDefaultConfiguration not implemented")
+}
+func (UnimplementedConfigServer) ResetConfiguration(context.Context, *ResetRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ResetConfiguration not implemented")
+}
+func (UnimplementedConfigServer) DryRun(context.Context, *DryRunRequest) (*DryRunResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DryRun not implemented")
+}
+func (UnimplementedConfigServer) ConfigurationHistory(context.Context, *driverutil.ConfigurationHistoryRequest) (*HistoryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ConfigurationHistory not implemented")
+}
+func (UnimplementedConfigServer) mustEmbedUnimplementedConfigServer() {}
+
+// UnsafeConfigServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ConfigServer will
+// result in compilation errors.
+type UnsafeConfigServer interface {
+	mustEmbedUnimplementedConfigServer()
+}
+
+func RegisterConfigServer(s grpc.ServiceRegistrar, srv ConfigServer) {
+	s.RegisterService(&Config_ServiceDesc, srv)
+}
+
+func _Config_GetDefaultConfiguration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(driverutil.GetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).GetDefaultConfiguration(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_GetDefaultConfiguration_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).GetDefaultConfiguration(ctx, req.(*driverutil.GetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_SetDefaultConfiguration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ConfigSpec)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).SetDefaultConfiguration(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_SetDefaultConfiguration_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).SetDefaultConfiguration(ctx, req.(*ConfigSpec))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_GetConfiguration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(driverutil.GetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).GetConfiguration(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_GetConfiguration_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).GetConfiguration(ctx, req.(*driverutil.GetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_SetConfiguration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ConfigSpec)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).SetConfiguration(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_SetConfiguration_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).SetConfiguration(ctx, req.(*ConfigSpec))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_ResetDefaultConfiguration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).ResetDefaultConfiguration(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_ResetDefaultConfiguration_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).ResetDefaultConfiguration(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_ResetConfiguration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ResetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).ResetConfiguration(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_ResetConfiguration_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).ResetConfiguration(ctx, req.(*ResetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_DryRun_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DryRunRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).DryRun(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_DryRun_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).DryRun(ctx, req.(*DryRunRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Config_ConfigurationHistory_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(driverutil.ConfigurationHistoryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ConfigServer).ConfigurationHistory(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Config_ConfigurationHistory_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ConfigServer).ConfigurationHistory(ctx, req.(*driverutil.ConfigurationHistoryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// Config_ServiceDesc is the grpc.ServiceDesc for Config service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Config_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "example.Config",
+	HandlerType: (*ConfigServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetDefaultConfiguration",
+			Handler:    _Config_GetDefaultConfiguration_Handler,
+		},
+		{
+			MethodName: "SetDefaultConfiguration",
+			Handler:    _Config_SetDefaultConfiguration_Handler,
+		},
+		{
+			MethodName: "GetConfiguration",
+			Handler:    _Config_GetConfiguration_Handler,
+		},
+		{
+			MethodName: "SetConfiguration",
+			Handler:    _Config_SetConfiguration_Handler,
+		},
+		{
+			MethodName: "ResetDefaultConfiguration",
+			Handler:    _Config_ResetDefaultConfiguration_Handler,
+		},
+		{
+			MethodName: "ResetConfiguration",
+			Handler:    _Config_ResetConfiguration_Handler,
+		},
+		{
+			MethodName: "DryRun",
+			Handler:    _Config_DryRun_Handler,
+		},
+		{
+			MethodName: "ConfigurationHistory",
+			Handler:    _Config_ConfigurationHistory_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
